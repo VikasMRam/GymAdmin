18cce8c4e2b83ee32d3f2b314f4d1b1dae7bb39d
Alfonso de la Osa <alfonso@seniorly.com>
2018-03-12 13:58:33 +0000

    fixed storybook webpack config

--- private/storybook/webpack.config.js
+++ private/storybook/webpack.config.js
@@ -1,18 +1,13 @@
-const path = require('path');
-
 const baseConfig = require('../../webpack.config');
 
-module.exports = storybookBaseConfig => {
-  console.log('output path', storybookBaseConfig.output);
-  return Object.assign({}, storybookBaseConfig, {
-    entry: Object.assign({}, storybookBaseConfig.entry, {
-      preview: ['babel-polyfill'].concat(storybookBaseConfig.entry.preview),
-    }),
-    resolve: Object.assign({}, storybookBaseConfig.resolve, {
-      modules: baseConfig.resolve.modules,
-    }),
-    module: Object.assign({}, storybookBaseConfig.module, {
-      rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
-    }),
-  });
-}
+module.exports = storybookBaseConfig => Object.assign({}, storybookBaseConfig, {
+  entry: Object.assign({}, storybookBaseConfig.entry, {
+    preview: ['babel-polyfill'].concat(storybookBaseConfig.entry.preview),
+  }),
+  resolve: Object.assign({}, storybookBaseConfig.resolve, {
+    modules: baseConfig.resolve.modules,
+  }),
+  module: Object.assign({}, storybookBaseConfig.module, {
+    rules: storybookBaseConfig.module.rules.concat(baseConfig.module.rules.slice(1)),
+  }),
+});
