bc31bee495862c21d88deca9739ec5f01b7a4ca0
Alfonso de la Osa <alfonso@seniorly.com>
2018-03-13 18:29:51 +0000

    button tests ok

--- src/components/atoms/Button/index.test.js
+++ src/components/atoms/Button/index.test.js
@@ -16,7 +16,7 @@ it('renders children when passed in', () => {
 });
 
 it('renders props when passed in', () => {
-  const wrapper = wrap({ type: 'submit' });
+  const wrapper = wrap({ buttonType: 'submit' });
   expect(wrapper.find({ type: 'submit' })).toHaveLength(1);
 });
 
