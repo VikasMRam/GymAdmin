cae6f0bfb8a92b9ebe5927c00747fdf4204345cd
Alfonso de la Osa <alfonso@seniorly.com>
2018-03-18 15:03:17 +0000

    moved Rating to atoms

--- src/components/molecules/AgentTile/index.js
+++ src/components/molecules/AgentTile/index.js
@@ -6,7 +6,7 @@ import { palette } from 'styled-theme';
 import Link from 'react-router-dom/Link';
 
 import { size } from 'sly/components/themes';
-import Rating from 'sly/components/molecules/Rating';
+import Rating from 'sly/components/atoms/Rating';
 import Avatar from 'sly/components/atoms/Avatar';
 
 const AgentDiv = styled.div`
