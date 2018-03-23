d1aff912637d9ebf26ec70768d484f06ca130e2a
Alfonso de la Osa <alfonso@seniorly.com>
2018-03-23 16:44:59 +0000

    removed log

--- src/components/organisms/ReduxField/index.js
+++ src/components/organisms/ReduxField/index.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Field from 'sly/components/molecules/Field';
 
 import * as molecules from 'sly/components/molecules';
-console.log('molecules', molecules);
 
 const ReduxField = ({ meta, input, ...props }) => {
   const fieldProps = {
