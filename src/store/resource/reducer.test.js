3b9eaf3d3d14c192301b3590f2981ce96f29b779
Alfonso de la Osa <fonz@seniorly.com>
2018-04-09 12:53:07 +0100

    fixed resource reducer tests

--- src/store/resource/reducer.test.js
+++ src/store/resource/reducer.test.js
@@ -21,7 +21,10 @@ const state = resourceState => ({
   },
 });
 
-describe.skip('resource reducer', () => {
+const item = id => ({id});
+const items = ids => ids.map(item);
+
+describe('resource reducer', () => {
   it('returns the initial state', () => {
       expect(reducer(undefined, {})).toEqual(initialState);
   });
@@ -56,14 +59,14 @@ describe.skip('resource reducer', () => {
     it('sets list in the initial state', () => {
       expect(reducer(
         initialState,
-        action(actions.RESOURCE_LIST_READ_SUCCESS, [1, 2, 3])
+        action(actions.RESOURCE_LIST_READ_SUCCESS, items([1, 2, 3]))
       )).toEqual(state({ list: [1, 2, 3] }));
     });
 
     it('overrides list in an existing state', () => {
       expect(reducer(
         state({ list: [1, 2, 3] }),
-        action(actions.RESOURCE_LIST_READ_SUCCESS, [3, 2, 1])
+        action(actions.RESOURCE_LIST_READ_SUCCESS, items([3, 2, 1]))
       )).toEqual(state({ list: [3, 2, 1] }));
     });
   });
@@ -83,13 +86,13 @@ describe.skip('resource reducer', () => {
 
   describe('RESOURCE_DETAIL_READ_SUCCESS', () => {
     it('sets detail in the initial state', () => {
-      expect(reducer(initialState, action(actions.RESOURCE_DETAIL_READ_SUCCESS, 1))).toEqual(state({ detail: 1 }));
+      expect(reducer(initialState, action(actions.RESOURCE_DETAIL_READ_SUCCESS, items([1])))).toEqual(state({ detail: 1 }));
     });
 
     it('overrides detail in an existing state', () => {
       expect(reducer(
         state({ detail: 1 }),
-        action(actions.RESOURCE_DETAIL_READ_SUCCESS, 2)
+        action(actions.RESOURCE_DETAIL_READ_SUCCESS, items([2]))
       )).toEqual(state({ detail: 2 }));
     });
   });
