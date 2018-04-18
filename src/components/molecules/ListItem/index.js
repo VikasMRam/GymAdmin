aaf0f31b9e7fd31290f6ac8987fefd0a458962c5
Pranesh <pranesh@seniorly.com>
2018-04-18 13:04:11 +0530

    Styling Correction on ListItem

--- src/components/molecules/ListItem/index.js
+++ src/components/molecules/ListItem/index.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import { string, number } from 'prop-types';
 import styled from 'styled-components';
 
-import { size } from 'sly/components/themes'
+import { size } from 'sly/components/themes';
 import Icon from 'sly/components/atoms/Icon';
 
 const ListItemDiv = styled.div`
   display: flex;
+  padding-top: ${size('spacing.large')};
 `;
 
-const ListItemIconDiv = styled.div`
-  padding-right: ${size('spacing.regular')}
-`;
+const ListItemIconDiv = styled.div``;
 
 const ListItemTextDiv = styled.div`
-  padding-top: ${size('spacing.small')}
+  padding-left: ${size('spacing.regular')};
 `;
-
 
 export default class ListItem extends Component {
   static propTypes = {
     text: string.isRequired,
-  }
+  };
   render() {
     const { text } = this.props;
     return (
