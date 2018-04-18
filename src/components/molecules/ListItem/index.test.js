8110e6d072c0029aa141c779081cce0eef7d5720
Pranesh <pranesh@seniorly.com>
2018-04-18 12:15:30 +0530

    Unit Tests for ListItem Molecule

---
+++ src/components/molecules/ListItem/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Icon from 'sly/components/atoms/Icon';
+import ListItem from '.';
+
+const wrap = () => shallow(<ListItem text="blah" />);
+
+it('renders text when passed in', () => {
+  const wrapper = wrap();
+  expect(wrapper.contains('blah')).toBe(true);
+});
+
+it('renders icon when text is passed in', () => {
+  const wrapper = wrap();
+  expect(wrapper.find(Icon)).toHaveLength(1);
+});
+
+it('renders icon when text is passed in', () => {
+  const wrapper = wrap();
+  expect(wrapper.find(Icon)).toHaveLength(1);
+});
+
+it('does not render children when passed in', () => {
+  const wrapper = wrap({ children: 'test' });
+  expect(wrapper.contains('test')).toBe(false);
+});
