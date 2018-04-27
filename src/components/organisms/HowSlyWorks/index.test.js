60d957bc14742881de186af9666fa03b5676f45b
Sushanth Ramakrishna <sushanthr@gmail.com>
2018-04-27 16:36:44 -0700

    Remove invalid tests

--- src/components/organisms/HowSlyWorks/index.test.js
+++ src/components/organisms/HowSlyWorks/index.test.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import CommunityDetails from '.';
+import HowSlyWorks from '.';
 
-const wrap = (props = {}) => mount(<CommunityDetails {...props} />);
+const wrap = (props = {}) => mount(<HowSlyWorks {...props} />);
 
-const communityName = 'testCommunityName';
-const communityDescription = 'communityDescription text here';
-const staffDescription = 'staffDescription text here';
-const residentDescription = 'residentDescription text here';
 
-describe('CommunityDetails', () => {
-  it('verify communityDescription is shown', () => {
-    const wrapper = wrap({
-      communityName, communityDescription,
-    });
-    expect(wrapper.text()).toContain(communityDescription);
-  });
+describe('HowSeniorlyWorks', () => {
 
-  it('verify staffDescription is shown', () => {
-    const wrapper = wrap({
-      communityName, communityDescription, staffDescription,
-    });
-    expect(wrapper.text()).toContain(communityDescription);
-    expect(wrapper.text()).toContain(staffDescription);
-  });
-
-  it('verify residentDescription is shown', () => {
-    const wrapper = wrap({
-      communityName, communityDescription, staffDescription, residentDescription,
-    });
-    expect(wrapper.text()).toContain(communityDescription);
-    expect(wrapper.text()).toContain(staffDescription);
-    expect(wrapper.text()).toContain(residentDescription);
-  });
 });
