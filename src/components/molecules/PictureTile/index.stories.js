d7e0f2facb63a1898644d26bcd6b776791b79523
Pranesh <pranesh@seniorly.com>
2018-05-01 16:31:57 +0530

    Basic Test for PictureTile

--- src/components/molecules/PictureTile/index.stories.js
+++ src/components/molecules/PictureTile/index.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import PictureTile from '.';
 
 import RhodaGoldmanPlaza from 'sly/../private/storybook/sample-data/property-rhoda-goldman-plaza.json';
+import PictureTile from '.';
 
 const { gallery } = RhodaGoldmanPlaza;
 const { images } = gallery;
