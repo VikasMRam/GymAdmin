826bbeff11e9cfefd0dc1d3d64e68aacc9369d78
Pranesh <pranesh@seniorly.com>
2018-05-04 12:03:33 +0530

    Basic Test for Footer

---
+++ src/components/molecules/Footer/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from '.';
+
+const wrap = (props = {}) => shallow(<Footer {...props} />);
+
+describe('Footer', () => {
+  it('does not renders children when passed in', () => {
+    const wrapper = wrap({ children: 'test' });
+    expect(wrapper.contains('test')).toBe(false);
+  });
+});
