c11b5c78876bd0e79a40d377a4b2af9dde24f0bd
Pranesh <pranesh@seniorly.com>
2018-05-17 14:21:10 +0530

    Additional Actions for SearchBox store

--- src/containers/HomePageContainer.js
+++ src/containers/HomePageContainer.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { object } from 'prop-types';
+import { object, func } from 'prop-types';
+import { connect } from 'react-redux';
 
 import HomePage from 'sly/components/pages/HomePage';
+import { setLocation } from 'sly/store/searchBox/actions';
 import { getPathFromPlacesResponse } from 'sly/services/helpers/search';
 
 class HomePageContainer extends Component {
   static propTypes = {
     history: object,
+    setLocation: func,
   };
 
   handleOnLocationSearch = (result) => {
-    const { history } = this.props;
+    const { history, setLocation } = this.props;
+    setLocation(result);
     const path = getPathFromPlacesResponse(result);
     history.push(path);
   };
@@ -20,4 +24,10 @@ class HomePageContainer extends Component {
   }
 }
 
-export default HomePageContainer;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setLocation: result => dispatch(setLocation(result)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(HomePageContainer);
--- src/store/searchBox/actions.test.js
+++ src/store/searchBox/actions.test.js
@@ -7,3 +7,17 @@ test('changeAddress', () => {
     payload: value,
   }));
 });
+
+test('setLocation', () => {
+  const value = 'San Francisco';
+  expect(actions.setLocation(value)).toEqual(expect.objectContaining({
+    type: actions.SEARCH_BOX_SET_LOCATION,
+    payload: value,
+  }));
+});
+
+test('clearLocation', () => {
+  expect(actions.clearLocation()).toEqual(expect.objectContaining({
+    type: actions.SEARCH_BOX_CLEAR_LOCATION,
+  }));
+});
