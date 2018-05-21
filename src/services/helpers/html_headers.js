8716bb5551675f7bd08264069024f8a549ba7f56
Sushanth Ramakrishna <sushanthr@gmail.com>
2018-05-21 12:55:43 -0700

    Structured data fixes

--- src/services/helpers/html_headers.js
+++ src/services/helpers/html_headers.js
@@ -2,106 +2,145 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import { host } from 'sly/config';
-import { tocs } from "sly/services/helpers/search";
+import { tocs } from 'sly/services/helpers/search';
+import { titleize } from 'sly/services/helpers/strings';
 
 
-
-
-export const getHelmetForSearchPage = ( { url, city, state, toc} ) => {
-  let actualToc = tocs.find((elem)=> (elem.value === toc));
-  let title = `See Search Results for ${actualToc.label} in ${city}, ${state}, and `;
-  let description = `Seniorly offers a comprehensive data search where we help maintain `;
-  let c_url = `${host}/${url.pathname}`;
-  let ld = {};
-  ld["@context"] = "http://schema.org";
-  ld["@type"] = "Webpage";
-  ld["@headline"] = title;
-  ld["url"] = c_url;
-
-  return (
-    <Helmet>
-      <title>{title}</title>
-      /*
-       OG Description
-       */
-      <meta content={description} property="og:description"/>
-      <meta content={title} property="og:title"/>
-      /**
-       Twitter description and title
-      */
-      <meta content={description} property="twitter:description"/>
-      <meta content={title} property="twitter:title"/>
-      <link rel={"canonical"} href={c_url}/>
-      <script type={"application/ld+json"}>{`${JSON.stringify(ld,stringifyReplacer)}`}</script>
-
-    </Helmet>
-  )
-};
-
-let stringifyReplacer = (k,v)=>{
+const stringifyReplacer = (k, v) => {
   if (k === 'hash' || k === 'key') {
     return undefined;
   }
   return v;
 };
 
-export const getHelmetForCommunityPage =  (community ) => {
-  const { name, address, propInfo, url } = community;
-  let toc = tocs.find((elem)=> (elem.label === propInfo.typeCare[0]));
-  let title = `${name} - Pricing, Photos and Floor Plans`;
-  let description =   `${name} ${toc} located at ${address}. See pricing and photos"`;
+const getSDForCommunity = ({
+  name, url, address, latitude, longitude, propRatings, startingRate, gallery = {},
+}) => {
+  const { reviewsValue, numReviews } = propRatings;
+  const ld = {};
+  ld['@context'] = 'http://schema.org';
+  ld['@type'] = 'LodgingBusiness';
+  ld.name = name;
+  ld.url = `${host}/${url}`;
 
-  let ld = getSDForCommunity({...community});
+  const addressLd = {};
+  addressLd['@type'] = 'PostalAddress';
+  addressLd.streetAddress = address.line1;
+  addressLd.addressLocality = address.city;
+  addressLd.addressRegion = address.state;
+  addressLd.addressCountry = address.country;
+  ld.address = addressLd;
+
+  const geo = {};
+  geo['@type'] = 'GeoCoordinates';
+  geo.latitude = latitude;
+  geo.longitude = longitude;
+  ld.geo = geo;
+
+  if (numReviews && numReviews > 0) {
+    const aggregatedRating = {};
+    aggregatedRating['@type'] = 'AggregatedRating';
+    aggregatedRating.ratingValue = reviewsValue;
+    aggregatedRating.ratingCount = numReviews;
+    ld.aggregatedRating = aggregatedRating;
+  }
+
+  if (startingRate > 0) {
+    ld.priceRange = `From ${startingRate} per month`;
+  }
+
+
+  let imageUrl = null;
+  if (gallery.images && gallery.images.length > 0) {
+    imageUrl = gallery.images[0];
+    const imageObj = {};
+    imageObj['@type'] = 'ImageObject';
+    imageObj.name = `Front Image for ${name}`;
+    imageObj.url = imageUrl;
+    ld.image = imageObj;
+  }
+
+  return ld;
+};
+
+
+export const getHelmetForSearchPage = ({
+  url, city, state, toc, communityList,
+}) => {
+  const actualToc = tocs.find(elem => (elem.value === toc));
+  const title = `See Search Results for ${actualToc.label} in ${titleize(city)}, ${titleize(state)}`;
+  const description = 'Seniorly offers a comprehensive data search where we help maintain ';
+  const canonicalUrl = `${host}/${url.pathname}`;
+  const ld = {};
+  ld['@context'] = 'http://schema.org';
+  ld['@type'] = 'Webpage';
+  ld['@headline'] = title;
+  ld.url = canonicalUrl;
+  const ldCommunities = [];
+  if (communityList.length > 0) {
+    communityList.map(e => ldCommunities.push(getSDForCommunity({ ...e })));
+  }
+
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+
+      <meta content={description} property="og:description" />
+      <meta content={title} property="og:title" />
+
+      <meta content={description} property="twitter:description" />
+      <meta content={title} property="twitter:title" />
+      <link rel="canonical" href={canonicalUrl} />
+
+      <script type="application/ld+json">{`${JSON.stringify(ld, stringifyReplacer)}`}</script>
+      {ldCommunities.length > 0 &&
+        <script type="application/ld+json">{`${JSON.stringify(ldCommunities, stringifyReplacer)}`}</script>
+      }
+    </Helmet>
+  );
+};
+
+
+export const getHelmetForCommunityPage = (community) => {
+  const {
+    name, address, propInfo, url, gallery = {}, videoGallery = {},
+  } = community;
+  const toc = tocs.find(elem => (elem.label === propInfo.typeCare[0]));
+  const title = `${name} - Pricing, Photos and Floor Plans`;
+  const description = `${name} ${toc} located at ${address}. See pricing and photos"`;
+  let imageUrl = null;
+  if (gallery.images && gallery.images.length > 0) {
+    imageUrl = gallery.images[0];
+  }
+  let videoUrl = null;
+
+  if (videoGallery.videos && videoGallery.videos.length > 0) {
+    videoUrl = videoGallery.video[0];
+  }
+
+  const ld = getSDForCommunity({ ...community });
 
   // TODO Add Image and Video and structured data.
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description}/>
-      /*
-       OG Description
-       */
-      <meta content={description} property="og:description"/>
-      <meta content={title} property="og:title"/>
-      /**
-       Twitter description and title
-      */
-      <meta content={description} property="twitter:description"/>
-      <meta content={title} property="twitter:title"/>
-      <link rel={"canonical"} href={url}/>
-      <script type={"application/ld+json"}>{`${JSON.stringify(ld,stringifyReplacer)}`}</script>
+      <meta name="description" content={description} />
+
+      <meta content={description} property="og:description" />
+      <meta content={title} property="og:title" />
+      <meta content={url} property="og:url" />
+      {imageUrl && <meta content={imageUrl} property="og:image" /> }
+      {videoUrl && <meta content={videoUrl} property="or:video" /> }
+
+      <meta content={description} property="twitter:description" />
+      <meta content={title} property="twitter:title" />
+      {imageUrl && <meta content={imageUrl} property="twitter:image:src" /> }
+
+
+      <link rel="canonical" href={url} />
+      <script type="application/ld+json">{`${JSON.stringify(ld, stringifyReplacer)}`}</script>
     </Helmet>
-  )
+  );
 };
 
-let getSDForCommunity = ( { name, url, address, latitude, longitude, reviewsValue, reviewsCount, size  }) =>{
-  let ld = {};
-  ld["@context"] = "http://schema.org";
-  ld["@type"] = "LodgingBusiness";
-  ld["name"] = name;
-  ld["url"] =`${host}/${url}`;
-
-  let addressLd = {};
-  addressLd["@type"] = "PostalAddress";
-  addressLd["streetAddress"] = address.line1;
-  addressLd["addressLocality"] = address.city;
-  addressLd["addressRegion"] = address.state;
-  addressLd["addressCountry"] = address.country;
-  ld["address"] =addressLd;
-
-  let geo = {};
-  geo["@type"] = "GeoCoordinates";
-  geo["latitude"] = latitude;
-  geo["longitude"] = longitude;
-  ld["geo"] = geo;
-
-  let aggregatedRating = {};
-  aggregatedRating["@type"] = "AggregatedRating";
-  aggregatedRating["ratingValue"]= reviewsValue;
-  aggregatedRating["ratingCount"] = reviewsCount;
-  ld["aggregatedRating"] = aggregatedRating;
-  //TODO Price Range and Description
-  ld["priceRange"] = "TODO";
-
-  return ld;
-};
--- src/services/helpers/strings.js
+++ src/services/helpers/strings.js
@@ -1,5 +1,5 @@
 
 export const titleize = (inputString) => {
-  //TODO EXTREMELY POOR IMPL. PLEASE USE LIBRARY
-  return inputString.split(/(\s|-)/g).filter( (e)=>(e !== "-" && e !== " ") ).map((elem) => { return `${elem[0].toUpperCase()}${elem.substring(1)}` }).join(' ');
+  // TODO EXTREMELY POOR IMPL. PLEASE USE LIBRARY
+  return inputString.split(/(\s|-)/g).filter(e => (e !== '-' && e !== ' ')).map((elem) => { return `${elem[0].toUpperCase()}${elem.substring(1)}`; }).join(' ');
 };
