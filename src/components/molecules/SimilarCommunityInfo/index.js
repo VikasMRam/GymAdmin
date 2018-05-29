0e2a5fa415864e6846d7e560e0a900f2721f8f83
Kunal Shah <knlshah@gmail.com>
2018-05-29 10:32:08 -0700

    language changes

--- src/components/molecules/SimilarCommunityInfo/index.js
+++ src/components/molecules/SimilarCommunityInfo/index.js
@@ -73,7 +73,7 @@ export default class SimilarCommunityInfo extends Component {
 
       <Rate>
         <NumberFormat value={startingRate} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-        {' per month.'}
+        {' per month'}
       </Rate>
     );
   }
