32b936325dfbf07eab6d0b351a7b851898fb5d15
Amal <amal@seniorly.com>
2018-05-30 18:32:59 +0530

    add tests for DiscoverHomeTile component

---
+++ src/components/molecules/DiscoverHomeTile/index.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import DiscoverHomeTile, { StyledImage, StyledHeading } from '.';
+import { Block, Button } from 'sly/components/atoms';
+
+const image =
+  'https://d1qiigpe5txw4q.cloudfront.net/uploads/dbede7dcc263e098e3705e818b5ff463/RGP-June-2014_sd.jpg';
+const title = 'test title';
+const description = 'test description';
+const buttonText = 'test buttonText';
+const onButtonClick = sinon.spy();
+
+const wrap = (props = {}) => shallow(<DiscoverHomeTile {...props} />);
+
+describe('DiscoverHomeTile', () => {
+  it('renders image', () => {
+    const wrapper = wrap({
+      image, title, description, buttonText, onButtonClick,
+    });
+    expect(wrapper.find(StyledImage)).toHaveLength(1);
+  });
+
+  it('renders title', () => {
+    const wrapper = wrap({
+      image, title, description, buttonText, onButtonClick,
+    });
+    expect(wrapper.find(StyledHeading)).toHaveLength(1);
+    expect(wrapper.find(StyledHeading).dive().dive().dive()
+      .text()).toBe(title);
+  });
+
+  it('renders description', () => {
+    const wrapper = wrap({
+      image, title, description, buttonText, onButtonClick,
+    });
+    expect(wrapper.find(Block)).toHaveLength(1);
+    expect(wrapper.find(Block).dive().text()).toBe(description);
+  });
+
+  it('renders buttonText', () => {
+    const wrapper = wrap({
+      image, title, description, buttonText, onButtonClick,
+    });
+    expect(wrapper.find(Button)).toHaveLength(1);
+    expect(wrapper.find(Button).dive().dive().text()).toBe(buttonText);
+  });
+
+  it('onButtonClick fires', () => {
+    const wrapper = wrap({
+      image, title, description, buttonText, onButtonClick,
+    });
+    wrapper.find(Button).simulate('click');
+    expect(onButtonClick.getCalls()).toHaveLength(1);
+  });
+});
