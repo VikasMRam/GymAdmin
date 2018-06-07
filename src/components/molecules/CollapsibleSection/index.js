7dd72b020dd9a0e8745b132d4181dd0313941149
Fonz <fonz@botverse.com>
2018-06-07 13:53:37 +0100

    fixed collapsible chevron

--- src/components/molecules/CollapsibleSection/index.js
+++ src/components/molecules/CollapsibleSection/index.js
@@ -119,7 +119,7 @@ export default class CollapsibleSection extends Component {
               <StyledHeading level={getHeadingLevel(size)}  size={getHeadingSize(size)}>
                 {title}
               </StyledHeading>
-              <Icon icon="chevron" palette="grays" flip={!collapsed} />
+              <Icon icon="chevron" size="large" palette="grays" flip={!collapsed} />
             </Header>
             <Content maxHeight={maxHeight} collapsed={collapsed}>
               <div ref={measureRef} {...props}>
