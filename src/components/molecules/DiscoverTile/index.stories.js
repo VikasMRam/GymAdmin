124540c536a18e10334da74397b419422bc654de
Pranesh <pranesh@seniorly.com>
2018-07-20 10:43:42 +0530

    Corrected Story of DiscoverTile

--- src/components/molecules/DiscoverTile/index.stories.js
+++ src/components/molecules/DiscoverTile/index.stories.js
@@ -4,16 +4,17 @@ import { storiesOf } from '@storybook/react';
 import { assetPath } from 'sly/components/themes';
 import DiscoverTile from '.';
 
-const props = {
+const content = {
   imageUrl: assetPath('images/how-it-works/discover-1.png'),
   heading: 'Families',
   subHeading: 'Find an assisted living community to love from our thousands of listings',
-  onClick() {
-    alert('Click on DiscoverTile')
-  },
 };
+
+function onClick() {
+  alert('Click on DiscoverTile');
+}
 
 storiesOf('Molecules|DiscoverTile', module)
   .add('default', () => (
-    <DiscoverTile {...props} />
+    <DiscoverTile content={content} onClick={onClick} />
   ));
