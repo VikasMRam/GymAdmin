8aa72d2c1753d79fd692d0fc433eb47bdc1edc6d
Pranesh <pranesh@seniorly.com>
2018-07-20 08:55:38 +0530

    DiscoverTile implementation

---
+++ src/components/molecules/DiscoverTile/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { palette } from 'styled-theme';
+import { string, func } from 'prop-types';
+
+import { size } from 'sly/components/themes';
+import { Image, Button } from 'sly/components/atoms';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  border: ${size('border.regular')} solid ${palette('grayscale', 2)};
+  border-radius: ${size('spacing.tiny')};
+  padding: 0 24px;
+  margin-top: 100px;
+
+  &:hover {
+    cursor: pointer;
+    background: #fff;
+    box-shadow: 0 ${size('spacing.regular')} ${size('spacing.large')} ${palette('grayscale', 0)}80;
+  }
+`;
+
+const ImageWrapper = styled(Image)`
+  margin: 0 auto;
+  margin-top: -100px;
+  margin-bottom: ${size('spacing.large')};
+`;
+
+const HeadingWrapper = styled.div`
+  margin: 0 auto;
+  font-size: ${size('text.subtitle')};
+  font-weight: bold;
+  margin-bottom: ${size('spacing.regular')};
+`;
+
+const SubheadingWrapper = styled.div`
+  font-size: ${size('text.subtitle')};
+  margin-bottom: ${size('spacing.xxxLarge')};
+  text-align: center;
+`;
+
+const StyledButton = styled(Button)`
+  margin: 0 auto;
+  margin-bottom: ${size('spacing.large')};
+`;
+
+const DiscoverTile = ({
+  imageUrl, heading, subheading, onClick,
+}) => {
+  return (
+    <Wrapper onClick={onClick}>
+      <ImageWrapper src={imageUrl} />
+      <HeadingWrapper>{heading}</HeadingWrapper>
+      <SubheadingWrapper>{subheading}</SubheadingWrapper>
+      <StyledButton>Learn More</StyledButton>
+    </Wrapper>
+  );
+};
+
+DiscoverTile.propTypes = {
+  imageUrl: string,
+  heading: string,
+  subheading: string,
+  onClick: func,
+};
+
+export default DiscoverTile;
---
+++ src/components/molecules/DiscoverTile/index.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { assetPath } from 'sly/components/themes';
+import DiscoverTile from '.';
+
+const props = {
+  imageUrl: assetPath('images/how-it-works/discover-1.png'),
+  heading: 'Families',
+  subheading: 'Find an assisted living community to love from our thousands of listings',
+  onClick() {
+    alert('Click on DiscoverTile')
+  },
+};
+
+storiesOf('Molecules|DiscoverTile', module)
+  .add('default', () => (
+    <DiscoverTile {...props} />
+  ));
