8a0ec53eb98c981741e03e5672f4dd75ec96037f
Pranesh <pranesh@seniorly.com>
2018-07-23 16:54:14 +0530

    IconInfoTile Molecule

---
+++ src/components/molecules/IconInfoTile/index.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import IconInfoTile from '.';
+
+const props = {
+  icon: 'search',
+  heading: 'Open Local Search Platform',
+  content: `We provide the most comprehensive, unbiased information on
+    senior housing and care without requiring personal data to access. Our 
+    service is 100% free for families and we list information on all
+    available communities regardless if we have a commercial relationship with them.`,
+};
+
+storiesOf('Molecules|IconInfoTile', module)
+  .add('default', () => <IconInfoTile {...props} />)
+  .add('borderless', () => <IconInfoTile {...props} borderless />);
