cf87c0adb337ca9c25654a1e3b36294549b2d7cc
Amal <amal@seniorly.com>
2018-08-08 12:55:18 +0530

    deploy notification correct pr url

--- .circleci/generate-deploy-status-message.sh
+++ .circleci/generate-deploy-status-message.sh
@@ -7,7 +7,7 @@ VERSION_FILE="$CURRENT_WORKING_DIRECTORY/VERSION"
 COMMIT_SHA_LINK=$CIRCLE_COMPARE_URL
 if [ -z "$COMMIT_SHA_LINK" ]; then
   PR_ID=`git log -n 1 | grep 'Merge pull request' | sed -e 's/.*#\([0-9]\+\).*/\1/'`
-  COMMIT_SHA_LINK="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${PR_ID}"
+  COMMIT_SHA_LINK="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pull/${PR_ID}"
 fi
 
 GIT_COMMIT_DESC=$(git log --format=oneline --pretty=format:"Commit SHA: <$COMMIT_SHA_LINK|%H>%nAuthor: %an%nCommitted on: %cd%nCommit message: %s" -n 1 $CIRCLE_SHA1)
