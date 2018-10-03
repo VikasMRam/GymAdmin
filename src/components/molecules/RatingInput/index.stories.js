3918b0b24b6a1f3e71c2d39fc5b0b6beae6bb6d6
Fonz <fonz@botverse.com>
2018-10-03 16:35:01 +0100

    rating input

---
+++ src/components/molecules/RatingInput/index.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import RatingInput from './';
+
+storiesOf('Molecules|RatingInput', module)
+  .add('default', () => <RatingInput />)
+  .add('disabled', () => (
+    <RatingInput disabled value={3.5} />
+  ))
+  .add('events', () => (
+    <RatingInput
+      onChange={action('change')}
+    />
+  ));
