76b002adcc24824467432b9436dd1e942836d97c
Amal <amal@seniorly.com>
2018-10-11 11:46:44 +0530

    add login form and signup form components

---
+++ src/components/organisms/LoginForm/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { func, bool } from 'prop-types';
+import { Field } from 'redux-form';
+import styled from 'styled-components';
+
+import { size } from 'sly/components/themes';
+
+import ReduxField from 'sly/components/organisms/ReduxField';
+
+import { Heading, Button, Block, Hr, Link } from 'sly/components/atoms';
+
+const Form = styled.form`
+  width: 100%;
+`;
+
+const StyledHeading = styled(Heading)`
+  margin-bottom: ${size('spacing.large')};
+`;
+
+const StyledButton = styled(Button)`
+  width: 100%;
+  margin-bottom: ${size('spacing.regular')};
+`;
+
+const StyledHr = styled(Hr)`
+  margin-top: ${size('spacing.xLarge')};
+  margin-bottom: ${size('spacing.xLarge')};
+`;
+
+const StyledReduxField = styled(ReduxField)`
+  display: flex;
+  align-items: baseline;
+`;
+
+const LoginForm = ({ handleSubmit, submitting, onSignupClicked }) => (
+  <Form onSubmit={handleSubmit}>
+    <StyledHeading level="subtitle" size="subtitle">Log in</StyledHeading>
+    <Field
+      name="email"
+      label="Email Address"
+      type="email"
+      placeholder="Email Address"
+      component={ReduxField}
+    />
+    <Field
+      name="password"
+      label="Password"
+      type="text"
+      placeholder="Password"
+      component={ReduxField}
+    />
+    <Field
+      name="rememberme"
+      label="Remember me"
+      type="checkbox"
+      responsive
+      component={StyledReduxField}
+    />
+    <StyledButton type="submit" kind="jumbo" disabled={submitting}>
+      Log in
+    </StyledButton>
+    <StyledHr />
+    <Block>Don&apos;t have an account? <Link to="/" onClick={onSignupClicked}>Sign up</Link></Block>
+  </Form>
+);
+
+LoginForm.propTypes = {
+  handleSubmit: func.isRequired,
+  submitting: bool,
+  onSignupClicked: func,
+};
+
+export default LoginForm;
