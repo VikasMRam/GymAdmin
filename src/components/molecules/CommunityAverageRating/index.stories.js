b60c7cda920e43405ce422974f1218114c5e9045
Pranesh <pranesh@seniorly.com>
2018-11-05 19:57:51 +0530

    Removing unnecessary Molecules

--- src/components/molecules/CommunityAverageRating/index.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import CommunityAverageRating from 'sly/components/molecules/CommunityAverageRating';
-
-storiesOf('Molecules|CommunityAverageRating', module)
-  .add('default', () => (
-    <CommunityAverageRating rating={3.6} />
-  ));
--- src/components/molecules/CommunityPricing/index.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import CommunityPricing from 'sly/components/molecules/CommunityPricing';
-
-storiesOf('Molecules|CommunityPricing', module)
-  .add('default', () => (
-    <CommunityPricing price={4300} />
-  ));
