c540b1cfaf66b876f61dddbf91e72c11f7ddb3f3
Pranesh <pranesh@seniorly.com>
2018-12-08 16:22:03 +0530

    Fixing Notes being shown when user already provided Name

--- src/components/organisms/CommunityBookATourContactForm/index.test.js
+++ src/components/organisms/CommunityBookATourContactForm/index.test.js
@@ -44,6 +44,7 @@ describe('CommunityBookATourContactForm', () => {
     expect(wrapper.contains(subheading)).toBe(true);
     expect(wrapper.find(Field).filter({ name: 'name' })).toHaveLength(0);
     expect(wrapper.find(Field).filter({ name: 'phone' })).toHaveLength(1);
+    expect(wrapper.find(Field).filter({ name: 'notes' })).toHaveLength(1);
 
     expect(wrapper.find(Block).filter({ palette: 'danger' })).toHaveLength(0);
     expect(wrapper.find(Link).filter({ href: '/tos' })).toHaveLength(1);
@@ -63,7 +64,6 @@ describe('CommunityBookATourContactForm', () => {
     const wrapper = wrap({ error });
     expect(wrapper.find(Field).filter({ name: 'name' })).toHaveLength(1);
     expect(wrapper.find(Field).filter({ name: 'phone' })).toHaveLength(1);
-    expect(wrapper.find(Field).filter({ name: 'notes' })).toHaveLength(1);
 
     expect(wrapper.find(Block).filter({ palette: 'danger' })).toHaveLength(1);
     expect(wrapper.find(Link).filter({ href: '/tos' })).toHaveLength(1);
