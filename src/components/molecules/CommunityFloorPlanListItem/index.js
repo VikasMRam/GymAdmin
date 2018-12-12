05d1fa281eb2f88aa4f281a04c6fef15a7348465
Pranesh <pranesh@seniorly.com>
2018-12-12 21:49:01 +0530

    Fixing Font weight medium

--- src/components/molecules/CommunityFloorPlanListItem/index.js
+++ src/components/molecules/CommunityFloorPlanListItem/index.js
@@ -88,7 +88,7 @@ const priceTypeMap = {
 };
 
 const StyledNumberFormat = styled(NumberFormat)`
-  font-weight: 500;
+  font-weight: ${size('weight.medium')};
 `;
 
 const CommunityFloorPlanListItem = ({
