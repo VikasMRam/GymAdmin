3140181a996493cf8509362839be781d21a08985
Pranesh <pranesh@seniorly.com>
2018-12-20 16:42:09 +0530

    Tablet Layout for AgentSummary

--- src/components/molecules/AgentSummary/index.js
+++ src/components/molecules/AgentSummary/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { string, number, arrayOf, func } from 'prop-types';
 import styled from 'styled-components';
+import NumberFormat from 'react-number-format';
 
 import { size } from 'sly/components/themes/index';
-import { Image, Icon, Block, Button, Span } from 'sly/components/atoms';
+import { Image, Icon, Block, Button, Span, Hr, Link } from 'sly/components/atoms';
 import { formatRating } from 'sly/services/helpers/rating';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    flex-direction: row;
+  }
 `;
 
 
@@ -20,23 +25,48 @@ const AgentImageWrapper = styled.div`
   display: block;
   margin: 0 auto;
   margin-bottom: ${size('spacing.xLarge')};
+
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    flex: 1;
+    img {
+      border-radius: initial;
+    }
+  }
 `;
 
 const TextSection = styled.div`
-  text-align: center;
   display: flex;
   flex-direction: column;
   margin: 0 ${size('spacing.large')};
+
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    flex: 1;
+  }
 `;
 
 const AgentName = styled(Block)`
   margin-bottom: ${size('spacing.regular')};
+  text-align: center;
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    text-align: left;
+  }
 `;
 
 const ReviewValueSection = styled.div`
-  margin: 0 auto;
   align-items: center;
   margin-bottom: ${size('spacing.xLarge')};
+  text-align: center;
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    text-align: left;
+  }
+`;
+
+const StyledHr = styled(Hr)`
+  display: none;
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    display: block;
+    margin-top: 0;
+  }
 `;
 
 const FamiliesHelpedSection = styled.div`
@@ -52,18 +82,28 @@ const AgentsCitiesSection = styled.div`
 const AskQuestionButton = styled(Button)`
   width: 100%;
   margin-bottom: ${size('spacing.xLarge')};
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    width: initial;
+    margin-right: ${size('spacing.xLarge')};
+  }
 `;
 
 const PhoneSection = styled.div`
+  text-align: center;
   margin-bottom: ${size('spacing.large')};
+  span {
+    margin-right: ${size('spacing.regular')};
+  }
 `;
 
-const PhoneIcon = styled(Icon)`
-  margin-right: ${size('spacing.regular')};
+const AskQuestionPhoneSection = styled.div`
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    display: flex;
+    align-items: center;
+  }
 `;
-
 const AgentSummary = ({
-  profileImageUrl, displayName, aggregateRating, numRatings, recentFamiliesHelped, citiesServed, slyPhone, onButtonClick,
+  profileImageUrl, displayName, aggregateRating, numRatings, recentFamiliesHelped, citiesServed, slyPhone, onButtonClick, onPhoneClick,
 }) => {
   const firstName = displayName.split(' ')[0];
   return (
@@ -75,11 +115,12 @@ const AgentSummary = ({
         <AgentName weight="medium" size="title">{displayName}</AgentName>
         {aggregateRating &&
           <ReviewValueSection>
-            <Span icon="star" size="regular" palette="secondary" />
+            <Icon icon="star" size="regular" palette="secondary" />
             <Span size="subtitle" weight="medium"> {formatRating(aggregateRating)} </Span>
             {numRatings && <Span size="caption" palette="grey">{numRatings} reviews</Span>}
           </ReviewValueSection>
         }
+        <StyledHr />
         {recentFamiliesHelped &&
           <FamiliesHelpedSection>
             <Span weight="medium">Families helped: </Span>
@@ -92,13 +133,23 @@ const AgentSummary = ({
             <Span>{citiesServed.join(', ')}</Span>
           </AgentsCitiesSection>
         }
-        <AskQuestionButton onClick={onButtonClick}>Ask a Question</AskQuestionButton>
-        {slyPhone &&
-          <PhoneSection>
-            <PhoneIcon icon="phone" size="regular" palette="primary" />
-            <Span size="subtitle" weight="medium" palette="primary">{slyPhone}</Span>
-          </PhoneSection>
-        }
+        <AskQuestionPhoneSection>
+          <AskQuestionButton onClick={onButtonClick}>Ask a Question</AskQuestionButton>
+          {slyPhone &&
+            <PhoneSection>
+              <Icon icon="phone" size="regular" palette="primary" />
+              <Link href={`tel:${slyPhone}`} onClick={onPhoneClick}>
+                <Span size="subtitle" weight="medium" palette="primary">
+                  <NumberFormat
+                    value={slyPhone}
+                    format="###-###-####"
+                    displayType="text"
+                  />
+                </Span>
+              </Link>
+            </PhoneSection>
+          }
+        </AskQuestionPhoneSection>
       </TextSection>
     </Wrapper>
   );
@@ -111,8 +162,9 @@ AgentSummary.propTypes = {
   numRatings: number,
   recentFamiliesHelped: number,
   citiesServed: arrayOf(string),
-  slyPhone: string,
+  slyPhone: number,
   onButtonClick: func,
+  onPhoneClick: func,
 };
 
 export default AgentSummary;
--- src/components/molecules/AgentSummary/index.stories.js
+++ src/components/molecules/AgentSummary/index.stories.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import styled from 'styled-components';
 
-import { size } from 'sly/components/themes/index';
 import AgentSummary from 'sly/components/molecules/AgentSummary';
 import { agents } from 'sly/services/helpers/agents';
 
@@ -15,15 +13,8 @@ const defaultProp = {
   aggregateRating: 3.53223232,
   recentFamiliesHelped: 20,
   citiesServed: ['Sausalito', 'Mill Valley', 'Tiburon', 'Belvedere', 'Corte Madera', 'Larkspur', 'Greenbrae', 'Kentfield', 'Ross', 'San Anselmo'],
-  slyPhone: '925-831-2050',
+  slyPhone: 9258312050,
 };
 
-const MobileWrapper = styled.div`
-  width: ${size('mobileLayout.col4')};
-  margin: 0 auto;
-`;
-
-
 storiesOf('Molecules|AgentSummary', module)
-  .add('default', () => <AgentSummary {...defaultProp} />)
-  .add('mobile', () => <MobileWrapper><AgentSummary {...defaultProp} /></MobileWrapper>);
+  .add('default', () => <AgentSummary {...defaultProp} />);
