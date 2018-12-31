46b4e911d38d3668f6db312fe1c418da29216f0f
Amal <amal@seniorly.com>
2018-12-31 12:56:56 +0530

    make Bar and PriceBar modifications

--- src/components/molecules/PriceBar/index.stories.js
+++ src/components/molecules/PriceBar/index.stories.js
@@ -9,4 +9,10 @@ const price = 1200;
 storiesOf('Molecules|PriceBar', module)
   .add('default', () => (
     <PriceBar width={width} price={price} />
+  ))
+  .add('with label', () => (
+    <PriceBar width={width} price={price} label="Assisted living within 20 miles" />
+  ))
+  .add('with label, variation and palette', () => (
+    <PriceBar width={width} price={price} label="Assisted living within 20 miles" palette="grey" variation="background" />
   ));
