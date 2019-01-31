61d29e89456dba994367e3fae219faeaec90eb6b
Fonz <fonz@botverse.com>
2019-01-31 11:52:15 +0000

    revert withServerState

--- src/store/withServerState.js
+++ src/store/withServerState.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { fetchState } from 'react-router-server';
 import { connect } from 'react-redux';
-import { func, bool, object, array, any } from 'prop-types';
+import { func, bool } from 'prop-types';
 import { isEqual, omit } from 'lodash';
 import { parse as parseSearch } from 'query-string';
 
 import { isBrowser, isServer } from 'sly/config';
-import { resourceDetailReadRequest } from 'sly/store/resource/actions';
-import { isResourceDetailRequestComplete } from 'sly/store/selectors';
 
 const ensureArray = ary => Array.isArray(ary) ? ary : [ary];
 
@@ -18,13 +16,6 @@ class ServerStateComponent extends Component {
     setServerState: func.isRequired,
     hasServerState: bool.isRequired,
     cleanServerState: func.isRequired,
-    history: object,
-    dispatch: func,
-    match: object,
-    location: object,
-    ignoreSearch: array,
-    ChildComponent: any,
-    isUserMeDone: bool,
   };
 
   componentWillMount() {
@@ -35,38 +26,20 @@ class ServerStateComponent extends Component {
       hasServerState,
       cleanServerState,
       history,
-      dispatch,
-      isUserMeDone,
     } = this.props;
 
     global.rhistory = history;
 
-    const dispatchFetch = () => {
-      if (isUserMeDone) {
-        return fetchData(this.props)
-          .catch(handleError);
-      }
+    const { match, location } = this.props;
 
-      return Promise.all([
-        dispatch(resourceDetailReadRequest('user', 'me'))
-          // handles 401 from user/me (guest users)
-          .catch((e) => {
-            if (e.response && e.response.status && e.response.status !== 401) {
-              console.error(`SSR GET ${e.response.url} error - ${e.toString()}`);
-            }
-          }),
-        fetchData(this.props)
-          .catch(handleError),
-      ]);
-    };
-
-    if (!hasServerState) {
+    if(!hasServerState) {
       if (isServer) {
-        dispatchFetch()
+        fetchData(this.props)
+          .catch(handleError)
           .then(setServerState)
           .catch(setServerState);
       } else {
-        dispatchFetch();
+        fetchData(this.props);
       }
     } else if (isBrowser) {
       cleanServerState();
@@ -74,14 +47,12 @@ class ServerStateComponent extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { match, location } = this.props;
-
-    if (match.url !== nextProps.match.url) {
+    if (this.props.match.url !== nextProps.match.url) {
       nextProps.fetchData(nextProps);
     } else {
       const { ignoreSearch = [] } = nextProps;
       const ignore = ensureArray(ignoreSearch);
-      const prev = omit(parseSearch(location.search), ignore);
+      const prev = omit(parseSearch(this.props.location.search), ignore);
       const next = omit(parseSearch(nextProps.location.search), ignore);
       if (!isEqual(prev, next)) {
         nextProps.fetchData(nextProps);
@@ -91,12 +62,12 @@ class ServerStateComponent extends Component {
 
   render() {
     const { ChildComponent, ...props } = this.props;
-    return <ChildComponent {...props} />;
+    return <ChildComponent {...props } />;
   }
 }
 
 const serverStateDecorator = fetchState(
-  (state) => {
+  state => {
     return {
       hasServerState: !!Object.keys(state).length,
       ...state,
@@ -109,7 +80,7 @@ const serverStateDecorator = fetchState(
 );
 
 const noop = () => ({});
-const passthrouh = _ => _;
+const passthrouh = _=>_;
 const promiseNoop = Promise.resolve;
 
 export default function withServerState({
@@ -129,13 +100,8 @@ export default function withServerState({
           }, {});
     };
 
-    const childMapStateToProps = state => ({
-      isUserMeDone: isResourceDetailRequestComplete(state, 'user'),
-    });
-
     const childMapDispatchToProps = (dispatch, props) => ({
       ...getMapDispatchToProps(dispatch, props),
-      dispatch,
       fetchData: (nextProps = props) => fetchData(dispatch, nextProps),
       handleError,
     });
@@ -143,7 +109,6 @@ export default function withServerState({
     const connector = connect(
       (state, ownProps) => ({
         ...mapStateToProps(state, ownProps),
-        ...childMapStateToProps(state, ownProps),
         ChildComponent,
       }),
       childMapDispatchToProps,
@@ -151,4 +116,5 @@ export default function withServerState({
 
     return serverStateDecorator(connector(ServerStateComponent));
   };
-}
+};
+
