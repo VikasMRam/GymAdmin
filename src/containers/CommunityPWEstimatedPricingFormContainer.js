a989536c6e1d45fcb5f41ade5a319fbfc24d2de6
Sushanth Ramakrishna <sushanthr@gmail.com>
2019-01-31 20:51:47 -0800

    Remove code

--- src/containers/CommunityPWEstimatedPricingFormContainer.js
+++ src/containers/CommunityPWEstimatedPricingFormContainer.js
@@ -1,13 +1,6 @@
 import { reduxForm } from 'redux-form';
 
 import CommunityPWEstimatedPricingForm from 'sly/components/organisms/CommunityPWEstimatedPricingForm';
-// import { createValidator, required } from 'sly/services/validation';
-
-// const validate = createValidator({
-//   roomType: [required],
-//   careType: [required],
-//   medicaidCoverage: [required],
-// });
 
 export default reduxForm({
   form: 'PWizardForm',
