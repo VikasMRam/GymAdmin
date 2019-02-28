cda3400b0d82084f047c82ce5130d43072355c95
Fonz <fonz@botverse.com>
2019-02-28 16:15:02 +0000

    rename test page in dashboard

--- src/components/DashboardApp.js
+++ src/components/DashboardApp.js
@@ -8,7 +8,7 @@ import smoothscroll from 'smoothscroll-polyfill';
 import theme from 'sly/components/themes/default';
 import setGlobalStyles from 'sly/components/themes/setGlobalStyles';
 import { assetPath } from 'sly/components/themes';
-import DashboardHomePageContainer from 'sly/containers/DashboardTestPageContainer';
+import DashboardTestPageContainer from 'sly/containers/DashboardTestPageContainer';
 import { routes as routesPropType } from 'sly/propTypes/routes';
 import Error from 'sly/components/pages/Error';
 import Router from 'sly/components/molecules/Router';
@@ -26,7 +26,7 @@ export default class App extends Component {
   static routes = [
     {
       path: DASHBOARD_PATH,
-      component: DashboardHomePageContainer,
+      component: DashboardTestPageContainer,
       exact: true,
     },
   ];
