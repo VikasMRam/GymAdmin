37a993a0c73a9b11ebb63f88d711144e6ad470c8
Pranesh <pranesh@seniorly.com>
2019-03-01 14:56:49 +0530

    Modifying createValidator to accept custom error messages

--- src/containers/DashboardProfileUserDetailsFormContainer.js
+++ src/containers/DashboardProfileUserDetailsFormContainer.js
@@ -3,11 +3,19 @@ import { reduxForm } from 'redux-form';
 import DashboardProfileUserDetailsForm from 'sly/components/organisms/DashboardProfileUserDetailsForm';
 import { createValidator, required, email, usPhone } from 'sly/services/validation/index';
 
+const emailWarning = 'Enter your email so your agent can help you by answering your questions and sending recommended communities.';
+const messageObj = {
+  email: {
+    required: emailWarning,
+  },
+};
+
 const warn = createValidator({
-  email: [required('Enter your email so your agent can help you by answering your questions and sending recommended communities.')],
-});
+  email: [required],
+}, messageObj);
 
 const validate = createValidator({
+  name: [required],
   email: [email],
   phone: [required, usPhone],
 });
