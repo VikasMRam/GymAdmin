1a3d45fcc4c955a887d0a5b5b12215babe1f635b
Pranesh <pranesh@seniorly.com>
2019-03-04 14:54:22 +0530

    Molecule for InputMessage

---
+++ src/components/molecules/InputMessage/index.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import InputMessage from 'sly/components/molecules/InputMessage';
+
+const name = 'blah';
+
+storiesOf('Molecules|InputMessage', module)
+  .add('default', () => <InputMessage name={`${name}Warning`} icon="warning" palette="warning" message="Warning" />)
+  .add('error', () => <InputMessage name={`${name}Error`} icon="close" palette="danger" message="Error" />);
