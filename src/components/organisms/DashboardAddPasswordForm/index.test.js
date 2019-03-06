e185038f6865f755b99fe4d0295161f6e359b423
Pranesh <pranesh@seniorly.com>
2019-03-06 12:34:34 +0530

    fixing test

--- src/components/organisms/DashboardAddPasswordForm/index.test.js
+++ src/components/organisms/DashboardAddPasswordForm/index.test.js
@@ -13,7 +13,6 @@ describe('DashboardAddPasswordForm', () => {
     expect(wrapper.find(Field).filter({ name: 'newPassword' })).toHaveLength(1);
     expect(wrapper.find(Field).filter({ name: 'confirmPassword' })).toHaveLength(1);
     expect(wrapper.find('FormSection')).toHaveLength(1);
-    expect(wrapper.find('StyledInputMessage')).toHaveLength(0);
   });
 
   it('render Create Password Warning', () => {
@@ -21,6 +20,5 @@ describe('DashboardAddPasswordForm', () => {
     expect(wrapper.find(Field).filter({ name: 'newPassword' })).toHaveLength(1);
     expect(wrapper.find(Field).filter({ name: 'confirmPassword' })).toHaveLength(1);
     expect(wrapper.find('FormSection')).toHaveLength(1);
-    expect(wrapper.find('StyledInputMessage')).toHaveLength(1);
   });
 });
