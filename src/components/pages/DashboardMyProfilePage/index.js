44bf0a721a4f92572a21e70934295f7a5d53693d
Pranesh <pranesh@seniorly.com>
2019-03-06 12:29:51 +0530

    Fixing bottom margin for User details form

--- src/components/pages/DashboardMyProfilePage/index.js
+++ src/components/pages/DashboardMyProfilePage/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { size } from 'sly/components/themes';
 import DashboardPageTemplate from 'sly/components/templates/DashboardPageTemplate';
 import DashboardProfileUserDetailsFormContainer from 'sly/containers/DashboardProfileUserDetailsFormContainer';
 import DashboardAddPasswordFormContainer from 'sly/containers/DashboardAddPasswordFormContainer';
 
+const ProfileUserDetailsFormWrapper = styled.div`
+  margin-bottom: ${size('spacing.xLarge')};
+`;
+
 const DashboardMyProfilePage = () => (
   <DashboardPageTemplate>
-    <DashboardProfileUserDetailsFormContainer />
+    <ProfileUserDetailsFormWrapper>
+      <DashboardProfileUserDetailsFormContainer />
+    </ProfileUserDetailsFormWrapper>
     <DashboardAddPasswordFormContainer />
   </DashboardPageTemplate>
 );
