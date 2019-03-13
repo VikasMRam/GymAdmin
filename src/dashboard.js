504de41978eabfaab4b50a54dfcc16e17359fdec
Fonz <fonz@botverse.com>
2019-03-13 14:53:19 +0000

    fix using temporarily old models in the dashboard

--- src/dashboard.js
+++ src/dashboard.js
@@ -10,17 +10,18 @@ import Modal from 'react-modal';
 import { createApi, ApiProvider } from 'sly/services/newApi';
 import configureStore from 'sly/store/configure';
 import { basename } from 'sly/config';
+import api from 'sly/services/api';
 import DashboardApp from 'sly/components/DashboardApp';
 
 Modal.setAppElement('#app');
 
 const initialState = window.__INITIAL_STATE__;
-const api = createApi();
-const store = configureStore(initialState);
+const newApi = createApi();
+const store = configureStore(initialState, { api: api.create() });
 
 const renderApp = () => (
   <Provider store={store}>
-    <ApiProvider api={api}>
+    <ApiProvider api={newApi}>
       <BrowserRouter basename={basename}>
         <DashboardApp />
       </BrowserRouter>
