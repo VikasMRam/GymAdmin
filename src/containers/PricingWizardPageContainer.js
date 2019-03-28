e439e31ba64d782f9310dd8165e787fda84bbeb3
Fonz <fonz@botverse.com>
2019-03-28 16:48:29 +0000

    reorganize PrizingWizardPageContainer

--- src/containers/PricingWizardPageContainer.js
+++ src/containers/PricingWizardPageContainer.js
@@ -20,7 +20,7 @@ const mapStateToProps = (state, { match }) => {
   const communitySlug = getCommunitySlug(match);
   return {
     user: getDetail(state, 'user', 'me'),
-    userAction: getDetail(state, 'userAction') || {},
+    userDetails: (getDetail(state, 'userAction') || {}).userDetails,
     community: getDetail(state, 'community', communitySlug),
   };
 };
@@ -78,73 +78,85 @@ const handleResponses = (responses, { location }, redirect) => {
 export default class PricingWizardPageContainer extends Component {
   static propTypes = {
     community: communityPropType,
+    userDetails: object,
     user: object,
-    userAction: object,
     postUserAction: func.isRequired,
     history: object.isRequired,
   };
 
-  render() {
+  submitUserAction = (data) => {
     const {
-      community, user, postUserAction, history, userAction,
+      community, postUserAction, userDetails,
     } = this.props;
 
-    if (!community || !userAction) {
+    // here remove only fields that will be populated by getUserDetailsFromUAAndForm
+    const {
+      name, phone, medicaidCoverage, budget, roomType, careType, contactByTextMsg, interest, ...restData
+    } = data;
+
+    const user = getUserDetailsFromUAAndForm({
+      userDetails,
+      formData: data,
+    });
+
+    const value = {
+      ...restData,
+      propertyIds: [community.id],
+      user,
+    };
+
+    if (!user.email && !user.phone) {
+      return Promise.resolve();
+    }
+
+    const payload = {
+      action: CUSTOM_PRICING,
+      value,
+    };
+
+    SlyEvent.getInstance().sendEvent({
+      action: 'pricing-requested',
+      category: eventCategory,
+      label: community.id,
+    });
+
+    return Promise.all([
+      postUserAction(payload),
+    ]);
+  };
+
+  handleComplete = (data, openConfirmationModal) => {
+    const {
+      community, history,
+    } = this.props;
+
+    return this.submitUserAction(data).then(() => {
+      history.push(community.url);
+      openConfirmationModal();
+    });
+  };
+
+  render() {
+    const {
+      community, user, userDetails,
+    } = this.props;
+
+    if (!community) {
       return null;
     }
-    const { id, url } = community;
 
-    const submitUserAction = (data) => {
-      // here remove only fields that will be populated by getUserDetailsFromUAAndForm
-      const {
-        name, phone, medicaidCoverage, budget, roomType, careType, contactByTextMsg, interest, ...restData
-      } = data;
-      const { userDetails } = userAction;
-      const user = getUserDetailsFromUAAndForm({
-        userDetails,
-        formData: data
-      });
-      const value = {
-        ...restData,
-        propertyIds: [id],
-        user,
-      };
-      if (!user.email && !user.phone) {
-        return;
-      }
-      const payload = {
-        action: CUSTOM_PRICING,
-        value,
-      };
-      const event = {
-        action: 'pricing-requested',
-        category: eventCategory,
-        label: id,
-      };
-      SlyEvent.getInstance()
-        .sendEvent(event);
-      return postUserAction(payload);
-    };
-
-    const handleComplete = (data, openConfirmationModal) => {
-      history.push(url);
-      openConfirmationModal();
-      return submitUserAction(data);
-    };
-
-    const userDetails = userAction ? userAction.userDetails : null;
     return (
       <ModalController>
         {({
-            show,
-            hide,
-          }) => (
+          show,
+          hide,
+        }) => (
           <PricingWizardPage
             community={community}
             user={user}
             userDetails={userDetails}
-            userActionSubmit={submitUserAction}
-            onComplete={handleComplete}
+            userActionSubmit={this.submitUserAction}
+            onComplete={this.handleComplete}
             showModal={show}
             hideModal={hide}
           />
