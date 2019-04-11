62da1dfdc49c34085af67a4fc926f5bc61f38571
Fonz <fonz@seniorly.com>
2019-04-11 15:42:58 +0200

    ApiProvider

--- src/services/newApi/withApi.js
+++ src/services/newApi/withApi.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { object } from 'prop-types';
 import hoistNonReactStatic from 'hoist-non-react-statics';
 
-import createApi from './createApi';
-
-const beesApi = createApi();
 
 export default function withApi(ChildComponent) {
-  const WithApi = props => <ChildComponent api={beesApi} {...props} />;
+  const WithApi = (props, context) => <ChildComponent api={context.api} {...props} />;
 
+  WithApi.contextTypes = { api: object };
   WithApi.displayName = `WithApi(${ChildComponent.displayName || ChildComponent.name || 'WithApi'})`;
-  WithApi.propTypes = { api: object };
   WithApi.WrappedComponent = ChildComponent;
 
   hoistNonReactStatic(WithApi, ChildComponent);
