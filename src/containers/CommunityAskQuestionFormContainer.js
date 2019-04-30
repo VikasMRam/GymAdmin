6e75702b5835f3316ac3ccf4fbe66b3f63d4beff
Fonz <fonz@botverse.com>
2019-04-30 19:41:00 +0100

    added uuidActions to questions and reviews

--- src/containers/CommunityAskQuestionFormContainer.js
+++ src/containers/CommunityAskQuestionFormContainer.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, SubmissionError } from 'redux-form';
 import { string, func, object } from 'prop-types';
+import { withRouter } from 'react-router';
 
 import { createValidator, required } from 'sly/services/validation';
 import CommunityAskQuestionForm from 'sly/components/organisms/CommunityAskQuestionForm';
 import Thankyou from 'sly/components/molecules/Thankyou';
-import { prefetch } from 'sly/services/newApi';
+import { prefetch, query } from 'sly/services/newApi';
+import { PROFILE_ASK_QUESTION } from 'sly/services/newApi/constants';
 
 const validate = createValidator({
   question: [required],
@@ -23,10 +25,14 @@ const mapDispatchToProps = (dispatch, { api }) => ({
   createQuestion: data => dispatch(api.createQuestion(data)),
 });
 
+@withRouter
+
 @prefetch('community', 'getCommunity', (req, { communitySlug }) => req({
   id: communitySlug,
   include: 'similar-communities,questions,agents',
 }))
+
+@query('createAction', 'createUuidAction')
 
 @connect(null, mapDispatchToProps)
 
@@ -39,11 +45,12 @@ export default class CommunityAskQuestionFormContainer extends Component {
     initialValues: object,
     parentSlug: string,
     showModal: func,
+    createAction: func,
   };
 
   handleOnSubmit = (values) => {
     const {
-      communitySlug, createQuestion, status, showModal, parentSlug,
+      communitySlug, createQuestion, status, showModal, parentSlug, createAction, match,
     } = this.props;
     const { question, name, email } = values;
     const payload = {
@@ -53,14 +60,29 @@ export default class CommunityAskQuestionFormContainer extends Component {
       email,
       parentSlug,
     };
-    return createQuestion(payload).then(() => {
-      showModal(<Thankyou />);
-      // Hacky way. to push created question into array for rerender
-      status.community.refetch();
-    }).catch(({ body }) => {
-      const errorMessage = body.errors[0].detail;
-      throw new SubmissionError({ _error: errorMessage });
-    });
+    return createQuestion(payload)
+      .then(({ body }) => createAction({
+        type: 'UUIDAction',
+        attributes: {
+          actionInfo: {
+            slug: communitySlug,
+            name,
+            email,
+            questionText: question,
+            questionId: body.data.id,
+          },
+          actionPage: match.url,
+          actionType: PROFILE_ASK_QUESTION,
+        },
+      }))
+      .then(() => {
+        showModal(<Thankyou />);
+        // Hacky way. to push created question into array for rerender
+        status.community.refetch();
+      }).catch(({ body }) => {
+        const errorMessage = body.errors[0].detail;
+        throw new SubmissionError({ _error: errorMessage });
+      });
   };
 
   render() {
