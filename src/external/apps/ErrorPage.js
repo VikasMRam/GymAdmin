a4fbff3b3d5b8b55a30ee467095e0b82bf615a9a
Amal <amal@seniorly.com>
2019-05-15 13:26:26 +0530

    error page styling fixes

--- src/external/apps/ErrorPage.js
+++ src/external/apps/ErrorPage.js
@@ -5,10 +5,9 @@ import { size, palette } from 'sly/components/themes';
 import textAlign from 'sly/components/helpers/textAlign';
 
 const Wrapper = textAlign(styled.div`
-  position: relative;
   background-color: ${palette('white', 'base')};
-  margin: calc(${size('breakpoint.tablet')}/4) auto;
-  content-align: center;
+  margin: auto;
+  margin-top: calc(${size('breakpoint.tablet')}/4);
 
   @media screen and (min-width: ${size('breakpoint.tablet')}) {
     width:${size('breakpoint.tablet')};
