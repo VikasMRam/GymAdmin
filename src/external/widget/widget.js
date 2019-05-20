d664901dcb4e0ab90aef3e0801fbc0313033cde2
Amal <amal@seniorly.com>
2019-05-20 16:17:27 +0530

    modify widget script to support search type

--- src/external/widget/widget.js
+++ src/external/widget/widget.js
@@ -24,82 +24,48 @@
     }
   }
 
-  function getParams(scriptName) {
-    // find all script tags
-    var scripts = document.getElementsByTagName('script');
-    // look through them trying to find script having src scriptName
-    for (var i = 0; i < scripts.length; i++) {
-      if (scripts[i].src.indexOf(scriptName) > -1) {
-        // get an array of key=value strings of params
-        var pa = scripts[i].src.split('?').pop().split('&');
-        // split each key=value into array, then construct js object
-        var p = {};
-        for (var j = 0; j < pa.length; j++) {
-          var kv = pa[j].split('=');
-          p[kv[0]] = kv[1];
-        }
-        return p;
-      }
-    }
-    // no scripts match
-    return null;
-  }
-
-  function getURLParams() {
-    // find all script tags
-    if (document.location && document.location.search) {
-      var qs =  document.location.search;
-      var pa = qs.split('?').pop().split('&');
-      // split each key=value into array, then construct js object
-      var p = {};
-      for (var j = 0; j < pa.length; j++) {
-        var kv = pa[j].split('=');
-        p[kv[0]] = kv[1];
-      }
-      return p;
-    }
-    // no match
-    return {};
-  }
+  // the public object available in global scope
+  this.SeniorlyWidget = {
+    version: null,
+    created: new Date().toString(),
+    instances: []
+  };
+  var global = this;
 
   var Seniorly = {
     cssId: 'seniorly-widget-style',
     context: {
       cssFileUrl: process.env.EXTERNAL_ASSET_URL + '/widget.css',
       jsFileUrl: process.env.EXTERNAL_ASSET_URL + '/widget.js',
-      iframeUrl: process.env.EXTERNAL_WIZARDS_ROOT_URL,
+      iframeUrl: process.env.EXTERNAL_URL,
       env: process.env.SLY_ENV,
       version: process.env.VERSION
     },
-    config: {},
-    widgetClassName: {
-      badge: 'seniorly-badge-widget',
-      overlay: 'seniorly-overlay-widget',
-      closeOverlay: 'seniorly-close-overlay-widget',
-      closeOverlayButton: 'seniorly-close-overlay-button-widget',
-      popup: 'seniorly-popup-widget'
-    },
     altClassNames: {
       scrollLocked: 'seniorly-page-scroll-locked',
-      popupOnClickorInline: 'seniorly-popup-or-inline-widget'
+      inline: 'seniorly-inline-widget',
+    },
+    widgetClassName: {
+      overlay: 'seniorly-overlay-widget',
+      closeOverlay: 'seniorly-close-overlay-widget'
     },
     validWidgetConfig: {
-      type: ['badge', 'popupOnClickorInline']
+      type: ['modal', 'inline'],
+      widgetType: ['wizards/caw', 'search']
     },
     defaultWidgetConfig: {
-      type: 'popupOnClickorInline'
+      widgetType: process.env.EXTERNAL_DEFAULT_WIDGET_TYPE
     },
     widgetConfigAttributes: {
       type: 'data-seniorly-widget',
+      widgetType: 'data-seniorly-widget-type',
       state: 'data-seniorly-state',
       city: 'data-seniorly-city',
       utm_campaign: 'data-seniorly-campaign',
       utm_source: 'data-seniorly-source',
       utm_medium: 'data-seniorly-medium',
-      pixel: 'data-seniorly-pixel'
-    },
-    configQueryParamKeys: {
-      type: 'widget_type'
+      pixel: 'data-seniorly-pixel',
+      height: 'data-seniorly-height'
     },
     log: {
       prefix: '[Seniorly Widget]',
@@ -122,23 +88,51 @@
     // as key and array of instances as value
     widgetInstances: {},
 
-    populateContextAndConfig: function() {
+    populateContext: function() {
       Seniorly.context.pageRoot = document.getElementsByTagName('html')[0];
-
-      var params = getParams(Seniorly.context.jsFileUrl);
-      Seniorly.config.type = Seniorly.defaultWidgetConfig.type;
-      if (params) {
-        Seniorly.config.type =
-          params[Seniorly.configQueryParamKeys.type] || Seniorly.defaultWidgetConfig.type;
-      }
     },
 
-    validateConfig: function() {
+    getInstances: function() {
+      var matches = document.querySelectorAll('[' + Seniorly.widgetConfigAttributes.type + ']');
+      var instances = [];
+      var configKeys = Object.keys(Seniorly.widgetConfigAttributes);
+
+      // old browsers don't have forEach method on Nodelist so...
+      Array.prototype.forEach.call(matches, function(m) {
+        var config = {};
+        Object.keys(m.attributes).forEach(function(a) {
+          var configKey;
+          for (var i = 0; i < configKeys.length; i++) {
+            if (Seniorly.widgetConfigAttributes[configKeys[i]] === m.attributes[a].name) {
+              configKey = configKeys[i];
+              break;
+            }
+          }
+          if (configKey) {
+            config[configKey] = m.attributes[a].value;
+          }
+        });
+        // default values
+        Object.keys(Seniorly.defaultWidgetConfig).forEach(function(c) {
+          if (!config[c]) {
+            config[c] = Seniorly.defaultWidgetConfig[c];
+          }
+        });
+        var i = {
+          config: config,
+          elem: m
+        };
+        instances.push(i);
+      });
+      return instances;
+    },
+
+    validateConfig: function(configHash) {
       var hasError = false;
-      Object.keys(Seniorly.config).forEach(function(config) {
-        if (!Seniorly.validWidgetConfig[config] ||
-          Seniorly.validWidgetConfig[config].indexOf(Seniorly.config[config]) === -1) {
-          Seniorly.log.error('Invalid config value: ' + Seniorly.config[config] + ' for config key: ' + config);
+      Object.keys(configHash).forEach(function(config) {
+        if (Seniorly.validWidgetConfig[config] &&
+          Seniorly.validWidgetConfig[config].indexOf(configHash[config]) === -1) {
+          Seniorly.log.error('Invalid config value: ' + configHash[config] + ' for config key: ' + config);
           hasError = true;
         }
       });
@@ -159,28 +153,29 @@
     },
 
     initWidget: function() {
-      Seniorly.populateContextAndConfig();
-      var hasError = Seniorly.validateConfig();
-      if (hasError) {
-        Seniorly.log.warn('Failed to initialize Seniorly widget');
-      } else {
+      Seniorly.populateContext();
+      var instances = Seniorly.getInstances();
+      if (instances.length) {
         Seniorly.injectStyles();
-        var widget = Seniorly.widgets[Seniorly.config.type]();
-        widget.build();
-        widget.insert();
       }
+      global.SeniorlyWidget.instances = instances;
+      global.SeniorlyWidget.version = Seniorly.context.version;
+      instances.forEach(function(instance, i) {
+        var hasError = Seniorly.validateConfig(instance.config);
+        if (hasError) {
+          Seniorly.log.warn('Failed to initialize Seniorly widget instance ' + (i + 1));
+        } else {
+          var widget = Seniorly.widgets[instance.config.type](instance);
+          widget.build();
+          widget.insert();
+        }
+      });
     }
-  };
-  // the public object available in global scope
-  this.SeniorlyWidget = {
-    version: Seniorly.context.version,
-    created: new Date().toString(),
-    config: Seniorly.config
   };
 
   /*
     SeniorlyWidget base object from which widgets are created.
-    anything that is injected to third party site is a widget.eg: popup, badge
+    anything that is injected to third party site is a widget.eg: popup, inline
     A SeniorlyWidget will have build, buildContent, insert and destroy lifecylce methods.
       * build - creates widget dom elements. this calls buildContent to fetch widget dom node.
       * buildContent - where widget's dom elements can be created. The function can return a dom node
@@ -199,7 +194,9 @@
     var childContent = this.buildContent(this.type);
     if (childContent || Seniorly.widgetClassName[this.type]) {
       this.widget = document.createElement('div');
-      this.widget.className = 'seniorly-widget ' + Seniorly.widgetClassName[this.type];
+      var className = Seniorly.widgetClassName[this.type] ?
+        Seniorly.widgetClassName[this.type] : 'seniorly-' + this.type + '-widget';
+      this.widget.className = 'seniorly-widget ' + className;
       this.widget.onclick = this.options.onClick;
       if (childContent) {
         this.widget.appendChild(childContent);
@@ -251,9 +248,17 @@
       return str.join('&');
     },
     generateIframe: function(widgetType, params) {
+      var skipAttributes = ['type', 'widgetType'];
+      var iframeParams = {}
       var t = document.createElement('iframe');
-      var qs = Seniorly.helpers.serialize(params);
-      t.src = Seniorly.context.iframeUrl + '/caw?fromWidgetType=' + widgetType +
+
+      Object.keys(params).forEach(function(p) {
+        if (skipAttributes.indexOf(p) === -1) {
+          iframeParams[p] = params[p];
+        }
+      });
+      var qs = Seniorly.helpers.serialize(iframeParams);
+      t.src = Seniorly.context.iframeUrl + '/' + params.widgetType + '?fromWidgetType=' + widgetType +
         (qs.length ? '&' + qs : '');
       t.width = '100%';
       t.height = '100%';
@@ -263,52 +268,27 @@
   };
 
   Seniorly.widgets = {
-    badge: function() {
-      var w = new SeniorlyWidget('badge', {
-        onClick: function() {
-          var w = Seniorly.widgets.popup();
+    modal: function(instance) {
+      var w = new SeniorlyWidget('modal');
+      w.buildContent = function() {
+        instance.elem.onclick = function() {
+          var w = Seniorly.widgets.popup(instance);
           w.build();
           w.insert(document.getElementsByClassName(Seniorly.widgetClassName.overlay)[0]);
-        }
-      });
-      w.buildContent = function() {
-        var t = document.createElement('span');
-        t.innerHTML = 'Care Assesment Wizard<br>Powered by Seniorly';
-        return t;
+        };
       };
       return w;
     },
-    popupOnClickorInline: function() {
-      var w = new SeniorlyWidget('popupOnClickorInline');
+    inline: function(instance) {
+      var w = new SeniorlyWidget('inline');
       w.buildContent = function() {
-        var matches = document.querySelectorAll('[' + Seniorly.widgetConfigAttributes.type + ']');
-        // old browsers don't have forEach method on Nodelist so...
-        Array.prototype.forEach.call(matches, function(match) {
-          var skipAttributes = ['type'];
-          var type = match.getAttribute(Seniorly.widgetConfigAttributes.type);
-          var params = getURLParams();
-          Object.keys(Seniorly.widgetConfigAttributes).forEach(function(attr) {
-            if (skipAttributes.indexOf(attr) === -1) {
-              var av = match.getAttribute(Seniorly.widgetConfigAttributes[attr]);
-              if (av) {
-                params[attr] = av;
-              }
-            }
-          });
-
-          if (type === 'modal') {
-            match.onclick = function() {
-              var w = Seniorly.widgets.popup(params);
-              w.build();
-              w.insert(document.getElementsByClassName(Seniorly.widgetClassName.overlay)[0]);
-            };
-          } else if (type === 'inline') {
-            var i = Seniorly.helpers.generateIframe('popupOnClickorInline', params);
-            match.innerHTML = '';
-            match.appendChild(i);
-            addClass(match, Seniorly.altClassNames.popupOnClickorInline);
-          }
-        });
+        var i = Seniorly.helpers.generateIframe('inline', instance.config);
+        instance.elem.innerHTML = '';
+        instance.elem.appendChild(i);
+        addClass(instance.elem, Seniorly.altClassNames.inline);
+        if (instance.config.height) {
+          instance.elem.style.height = instance.config.height + 'px';
+        }
       };
       return w;
     },
@@ -337,7 +317,7 @@
           // it's a fair assumption that only one instance of overlay widget will be active
           Seniorly.widgetInstances['overlay'][0].destroy();
         }
-      })
+      });
       w.buildContent = function() {
         var t = document.createElement('button');
         t.type = 'button';
@@ -346,9 +326,9 @@
       };
       return w;
     },
-    popup: function(params) {
+    popup: function(instance) {
       var options = {
-        iframeParams: params
+        iframeParams: instance.config
       };
       var w = new SeniorlyWidget('popup', options);
       w.buildContent = function() {
