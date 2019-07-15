d9ffd1ae78e6dea06bccbda41ae24c4100c4bc6a
Pranesh <pranesh@seniorly.com>
2019-07-15 13:02:38 +0530

    Correcting logic in new container

--- src/containers/ConversationMessagesContainer.js
+++ src/containers/ConversationMessagesContainer.js
@@ -80,20 +80,7 @@ export default class ConversationMessagesContainer extends Component {
   }
 
   componentDidUpdate() {
-    if (!this.timeoutInst) {
-      const {
-        messages, conversation, user,
-      } = this.props;
-      const parsedLastestMessageCreatedAt = dayjs(messages[0].createdAt).utc();
-      const { conversationParticipants } = conversation;
-      const { id: userId } = user;
-      const viewingAsParticipant = conversationParticipants.find(p => p.participantID === userId);
-      const parsedViewedCreatedAt = dayjs(viewingAsParticipant.stats.lastReadMessageAt).utc();
-
-      if (parsedLastestMessageCreatedAt.isAfter(parsedViewedCreatedAt)) {
-        this.timeoutInst = setTimeout(this.updateLastReadMessageAt, MESSAGES_UPDATE_LAST_READ_TIMEOUT);
-      }
-    }
+    this.checkAndPatchLastReadMessage(MESSAGES_UPDATE_LAST_READ_TIMEOUT);
   }
 
   componentWillUnmount() {
@@ -111,6 +98,10 @@ export default class ConversationMessagesContainer extends Component {
     const { id } = conversation;
     if (message.payload.conversationId === id) {
       status.messages.refetch();
+      // Patch last read message immediately if the user is active on that conversation
+      if (document.hidden) {
+        this.checkAndPatchLastReadMessage(0);
+      }
       // prevent more handlers to be called if page is visible
       return document.hidden;
     }
@@ -148,6 +139,23 @@ export default class ConversationMessagesContainer extends Component {
 
     return hasFinished;
   };
+
+  checkAndPatchLastReadMessage(timeout) {
+    if (!this.timeoutInst) {
+      const {
+        messages, conversation, user,
+      } = this.props;
+      const parsedLastestMessageCreatedAt = dayjs(messages[0].createdAt).utc();
+      const { conversationParticipants } = conversation;
+      const { id: userId } = user;
+      const viewingAsParticipant = conversationParticipants.find(p => p.participantID === userId);
+      const parsedViewedCreatedAt = dayjs(viewingAsParticipant.stats.lastReadMessageAt).utc();
+
+      if (parsedLastestMessageCreatedAt.isAfter(parsedViewedCreatedAt)) {
+        this.timeoutInst = setTimeout(this.updateLastReadMessageAt, timeout);
+      }
+    }
+  }
 
   updateLastReadMessageAt = () => {
     const {
