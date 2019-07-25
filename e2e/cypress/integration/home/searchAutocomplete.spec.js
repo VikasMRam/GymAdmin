42f5f3f48cccaa52c4dfe1f3438c3d853c11c492
Fonz <fonz@botverse.com>
2019-07-25 13:36:43 +0100

    added cypress

---
+++ e2e/cypress/integration/home/searchAutocomplete.spec.js
@@ -0,0 +1,17 @@
+describe('Marketplace Home Page', () => {
+  it('AutoComplete City Search', () => {
+    cy.visit('http://www.teamseniorly.com');
+
+    cy.contains('Find a Home to Love');
+
+    cy.get('input[class*=SearchBox__SearchTextBox]')
+      .type('San Francisco, CA, USA');
+
+    cy.get('div[class*=SearchBox__SearchSuggestion]')
+      .should('have.length', 2)
+      .first()
+      .click();
+
+    cy.url().should('have.string', '/assisted-living/california/san-francisco');
+  });
+});
