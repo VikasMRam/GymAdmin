0e98057b1c4300d0575df2a2d94ff698834a6e92
Pranesh <pranesh@seniorly.com>
2019-07-31 16:37:52 +0530

    Trimming form data to avoid space in values

--- src/services/helpers/forms.js
+++ src/services/helpers/forms.js
@@ -6,3 +6,12 @@ export const withPreventDefault = handler => (e) => {
 export const selectFormData = (state, form, defaultValue = null) => (!state.form || !state.form[form])
   ? defaultValue
   : state.form[form].values || defaultValue;
+
+export const trimFormData = (data) => {
+  Object.keys(data).forEach((key) => {
+    const value = data[key];
+    if (typeof value === 'string' || value instanceof String) {
+      data[key] = value.trim();
+    }
+  });
+};
