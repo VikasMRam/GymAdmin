3ccab18dce3963e7365345747c7a62ef37d26266
Fonz <fonz@botverse.com>
2019-09-25 16:25:20 +0100

    ...and the constant

--- src/constants/dashboardAppPaths.js
+++ src/constants/dashboardAppPaths.js
@@ -16,7 +16,6 @@ const familyTypes = [
 export const FAMILY_DASHBOARD_FAVORITES_PATH = `${FAMILY_DASHBOARD_PATH}/favorites`;
 export const FAMILY_DASHBOARD_PROFILE_PATH = `${FAMILY_DASHBOARD_PATH}/my-profile`;
 export const AGENT_DASHBOARD_FAMILIES_PATH = `${AGENT_DASHBOARD_PATH}/my-families/:clientType(${familyTypes.join('|')})?`;
-export const AGENT_DASHBOARD_FAMILIES_NEW_PATH = `${AGENT_DASHBOARD_PATH}/my-families/new`;
 export const AGENT_DASHBOARD_FAMILIES_DETAILS_PATH = `${AGENT_DASHBOARD_PATH}/my-families/:id/:tab?`;
 export const AGENT_DASHBOARD_TASKS_PATH = `${AGENT_DASHBOARD_PATH}/tasks`;
 export const FAMILY_DASHBOARD_MESSAGES_PATH = `${FAMILY_DASHBOARD_PATH}/messages`;
