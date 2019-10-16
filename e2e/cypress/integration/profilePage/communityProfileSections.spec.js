346261ad7fb87f3028ee44b22cb1acf9756513d0
Fonz <fonz@botverse.com>
2019-10-16 18:12:33 +0100

    response from user-share

--- e2e/cypress/integration/profilePage/communityProfileSections.spec.js
+++ e2e/cypress/integration/profilePage/communityProfileSections.spec.js
@@ -26,6 +26,37 @@ describe('Community Profile Sections', () => {
       select('#concierge-number').should(($div) => {
         expect($div.text().replace(/[^\d]/g, '')).to.equal(data.twilioNumber.numbers[0].toString());
       });
+
+      select('.CommunityPricingAndRating').should('contain', data.propInfo.ratesText)
+
+      const rating = data.propRatings.reviewsValue.toFixed(1).replace(/\.0+$/, '');
+      select('.CommunityPricingAndRating').should('contain', rating);
+    });
+
+    it.only('should be able to share', () => {
+      cy.server();
+
+      cy.route('POST', '**/user-shares').as('postUserShares');
+
+      cy.visit(`/assisted-living/california/san-francisco/${data.id}`);
+      cy.get('button').contains('Share').click();
+      select('.ReactModal', 'h2').contains('Share this community').should('exist')
+
+      cy.get('form input[name="to"]').type('fonz@seniorly.com');
+      cy.get('form input[name="from"]').type('fonz@botverse.com');
+      cy.get('form textarea[name="message"]').type('check out this property');
+
+      cy.get('form button').contains('Send').click();
+
+      cy.wait('@postUserShares').then(async xhr => {
+        const text = await xhr.response.body.text();
+        const json = JSON.parse(text);
+        // expect(json).toEqual({
+        //
+        // });
+      });
+
+      select('.Notifications').contains('Community has been shared').should('exist');
     });
   });
 });
