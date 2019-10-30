085e86220baaad9ba548dd2673d84cf097a266a7
Amal <amal@seniorly.com>
2019-10-30 16:25:38 +0530

    add displayName modifier to shadow helper

--- src/components/helpers/shadow.js
+++ src/components/helpers/shadow.js
@@ -1,20 +1,21 @@
 import styled, { css } from 'styled-components';
 
 import { size, palette } from 'sly/components/themes';
+import { setDisplayName } from 'sly/components/helpers';
 import transition from 'sly/components/helpers/transition';
 
 const styles = (spread = 'regular') => css`
   box-shadow: 0 0 ${size('spacing', spread)} ${palette('slate', 'filler')}80;
 `;
 
-const shadow = (Component, ...props) => styled(Component)`
+const shadow = (Component, ...props) => setDisplayName(styled(Component)`
   ${styles(...props)}
-`;
+`, Component.displayName);
 
-export const shadowOnHover = (Component, ...props) => styled(transition(Component, 'box-shadow'))`
+export const shadowOnHover = (Component, ...props) => setDisplayName(styled(transition(Component, 'box-shadow'))`
   :hover {
     ${styles(...props)}
   }
-`;
+`, Component.displayName);
 
 export default shadow;
