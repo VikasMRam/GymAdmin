6d786022d5e90965f08725cb70c8dfde58d86d2b
Pranesh <pranesh@seniorly.com>
2019-10-30 15:50:09 +0530

    adding back json parse

--- e2e/cypress/helpers/getCommunity.js
+++ e2e/cypress/helpers/getCommunity.js
@@ -2,5 +2,5 @@ import buildEntity from './buildEntity';
 
 export const getCommunity = (community) => {
   const url = `/v0/marketplace/communities/${community}?include=similar-communities%2Cquestions%2Cagents`;
-  return cy.request(url).then(response => buildEntity((response.body)));
+  return cy.request(url).then(response => buildEntity(JSON.parse(response.body)));
 };
