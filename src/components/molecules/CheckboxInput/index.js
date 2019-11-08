54fc6e5cd591e6f480b2918a830a158d70afbd00
Pranesh <pranesh@seniorly.com>
2019-11-08 11:49:26 +0530

    Checking value if array before assigning to state in Checkbox input

--- src/components/molecules/CheckboxInput/index.js
+++ src/components/molecules/CheckboxInput/index.js
@@ -27,7 +27,7 @@ class CheckboxInput extends Component {
   constructor(props) {
     super(props);
     const { value } = props;
-    if (value) {
+    if (value && value instanceof Array) {
       this.state = { value };
     }
   }
