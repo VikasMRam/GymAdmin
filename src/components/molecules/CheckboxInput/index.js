ae7880d4a14a71fbef07af2362f97bb840713179
Pranesh <pranesh@seniorly.com>
2019-11-08 13:27:27 +0530

    using Array.isArray(value)

--- src/components/molecules/CheckboxInput/index.js
+++ src/components/molecules/CheckboxInput/index.js
@@ -27,7 +27,7 @@ class CheckboxInput extends Component {
   constructor(props) {
     super(props);
     const { value } = props;
-    if (value && value instanceof Array) {
+    if (Array.isArray(value)) {
       this.state = { value };
     }
   }
