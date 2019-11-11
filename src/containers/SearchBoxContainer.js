5812d199b4b67b8b3f43cb2064666e27c89ccf5e
Pranesh <pranesh@seniorly.com>
2019-11-11 19:22:38 +0530

    Using SearchBoxContainer but with connectController

--- src/containers/SearchBoxContainer.js
+++ src/containers/SearchBoxContainer.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import { string, func, object, bool } from 'prop-types';
-import { connect } from 'react-redux';
 import { geocodeByAddress } from 'react-places-autocomplete';
-import { withRouter } from 'react-router-dom';
 
 import { gMapsApiKey, loadAutoComplete } from 'sly/config';
-import { changeAddress, setLocation, clearLocation } from 'sly/store/actions';
-import { searchBoxAddress, searchBoxLocation } from 'sly/store/selectors';
-import {
-  filterLinkPath,
-  getSearchParamFromPlacesResponse,
-} from 'sly/services/helpers/search';
 import SearchBox from 'sly/components/molecules/SearchBox';
 import SlyEvent from 'sly/services/helpers/events';
-import {withRedirectTo} from "sly/services/redirectTo";
+import { connectController } from 'sly/controllers';
 
 class SearchBoxContainer extends Component {
   static propTypes = {
@@ -21,13 +13,11 @@ class SearchBoxContainer extends Component {
     address: string,
     defaultAddress: string,
     location: object,
-    changeAddress: func,
-    setLocation: func,
-    clearLocation: func,
+    set: func,
+    resetController: func,
     clearLocationOnBlur: bool,
     onTextChange: func,
     onLocationSearch: func,
-    redirectTo: func.isRequired,
     allowOnlySelectionFromSuggestions: bool,
   };
 
@@ -41,7 +31,7 @@ class SearchBoxContainer extends Component {
   }
 
   componentDidMount() {
-    const { changeAddress, defaultAddress } = this.props;
+    const { set, defaultAddress } = this.props;
     const scriptjs = require('scriptjs');
     if (loadAutoComplete) {
       scriptjs(
@@ -54,35 +44,37 @@ class SearchBoxContainer extends Component {
       );
 
       if (defaultAddress) {
-        changeAddress(defaultAddress);
+        set({ defaultAddress });
       }
     }
-
   }
+
   componentWillUnmount() {
+    const { resetController } = this.props;
     this.setState({
       isMounted: false,
     });
+    resetController();
   }
 
   handleBlur = () => {
     const { addressSelected } = this;
-    const { changeAddress, defaultAddress, address } = this.props;
+    const { set, defaultAddress, address } = this.props;
     if (address && address !== addressSelected) {
-      changeAddress(defaultAddress);
+      set({ defaultAddress });
     }
   };
 
   handleChange = (address) => {
-    const { changeAddress, onTextChange } = this.props;
+    const { onTextChange, set } = this.props;
     if (onTextChange) {
       onTextChange(address);
     }
-    changeAddress(address);
+    set({ address });
   };
 
   handleSelect = (address) => {
-    const { setLocation, address: value } = this.props;
+    const { set, address: value } = this.props;
     this.addressSelected = address;
     geocodeByAddress(address)
       .then(results => results[0])
@@ -94,7 +86,7 @@ class SearchBoxContainer extends Component {
         SlyEvent.getInstance().sendEvent({
           action: 'googleSearchTyped', category: result.formatted_address, label: value,
         });
-        setLocation(result);
+        set({ location: result });
         this.handleOnLocationSearch(result);
       })
       .catch(error => console.error('Error', error));
@@ -110,18 +102,14 @@ class SearchBoxContainer extends Component {
   };
 
   handleTextboxFocus = () => {
-    const { clearLocation } = this.props;
-    clearLocation();
+    const { set } = this.props;
+    set({ location: null });
   };
 
   handleOnLocationSearch = (result) => {
-    const { onLocationSearch, redirectTo } = this.props;
+    const { onLocationSearch } = this.props;
     if (onLocationSearch) {
       onLocationSearch(result);
-    } else {
-      const searchParams = getSearchParamFromPlacesResponse(result);
-      const { path } = filterLinkPath(searchParams);
-      redirectTo(path);
     }
   };
 
@@ -164,16 +152,11 @@ class SearchBoxContainer extends Component {
   }
 }
 
-const mapStateToProps = (state, { address }) => ({
-  address: searchBoxAddress(state),
+const mapStateToProps = (state, { controller = {}, address }) => ({
+  address: controller.address || '',
+  location: controller.location || null,
   defaultAddress: address,
-  location: searchBoxLocation(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  changeAddress: value => dispatch(changeAddress(value)),
-  setLocation: value => (dispatch(setLocation(value))),
-  clearLocation: () => dispatch(clearLocation()),
-});
 
-export default withRedirectTo(withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchBoxContainer)));
+export default connectController(mapStateToProps, null)(SearchBoxContainer);
