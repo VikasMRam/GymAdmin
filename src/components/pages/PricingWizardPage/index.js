041eebea93dc557a7450ac7c14e5c3cd1e80e559
Pranesh <pranesh@seniorly.com>
2019-11-11 17:20:48 +0530

    es lint fixes

--- src/components/pages/PricingWizardPage/index.js
+++ src/components/pages/PricingWizardPage/index.js
@@ -178,11 +178,11 @@ class PricingWizardPage extends Component {
 
   render() {
     const {
-      handleRoomTypeChange, handleCareTypeChange, handleBudgetChange, handleStepChange
+      handleRoomTypeChange, handleCareTypeChange, handleBudgetChange, handleStepChange,
     } = this;
 
     const {
-      community, user, uuidAux, userHas, onComplete, match, redirectTo
+      community, user, uuidAux, userHas, onComplete, match, redirectTo,
     } = this.props;
 
     const { id, mainImage, name } = community;
@@ -281,20 +281,20 @@ class PricingWizardPage extends Component {
             }}
         </WizardController>
         <Route path={`${match.url}/thank-you`}>
-          {(routeProps) => (
+          {routeProps => (
             <Modal isOpen={!!routeProps.match} onClose={() => redirectTo(community.url)} closeable>
               <CommunityWizardAcknowledgementContainer
-                heading='Thank you! Our team will be calling you from (855) 855-2629.'
-                subheading='We received your request and your Seniorly Partner Agent will work with you to get your exact pricing and availability.'
+                heading="Thank you! Our team will be calling you from (855) 855-2629."
+                subheading="We received your request and your Seniorly Partner Agent will work with you to get your exact pricing and availability."
                 similarCommunities={community.similarProperties}
                 buttonTo={FAMILY_DASHBOARD_FAVORITES_PATH}
-                type='pricingWizard'
+                type="pricingWizard"
               />
             </Modal>
           )}
         </Route>
         <Route path={`${match.url}/help`}>
-          {(routeProps) => (
+          {routeProps => (
             <Modal isOpen={!!routeProps.match} onClose={() => redirectTo(match.url)} closeable>
               <AdvisorHelpPopup onButtonClick={() => redirectTo(match.url)} />
             </Modal>
