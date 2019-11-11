2b917e60effe46d6d24a6a85e825241ae1d95728
Ed O'Reilly <ed@seniorly.com>
2019-11-11 17:49:09 +0000

    Optimizing how we display ratings. We used to use ~40 for each rating. Not we use at most 10.

--- src/components/molecules/Rating/index.js
+++ src/components/molecules/Rating/index.js
@@ -1,67 +1,72 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { number, string, oneOf } from 'prop-types';
 import styled from 'styled-components';
-import { prop } from 'styled-tools';
 
-import { palette } from 'sly/components/themes';
-import { Icon } from 'sly/components/atoms';
+import { palette, size } from 'sly/components/themes';
 
 const times = (nr, fn) => Array.from(Array(nr).keys()).map((_, i) => fn(i));
 
-const getWidth = (current, total) => {
-  if (total > current + 1) return 100;
-  else if (total < current) return 0;
-  return ((total - current) * 80) + 10;
-};
+const iconSize = props => size('icon', props.size);
 
 const Wrapper = styled.div`
   display: flex;
   line-height: 1;
 `;
 
-const BaseIcon = styled(Icon)`
-  svg {
-    color: ${palette('filler')};
-  }
+const StyledStar = styled.svg`
+  width: 5rem;
+  height: ${iconSize};
 `;
 
-export const PositionedMask = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  overflow: hidden;
-  width: ${prop('width')}%;
-
-  svg {
-    color: ${palette('base')};
-  }
+const BaseStarPath = styled.path`
+  color: ${palette('base')};
 `;
 
-const Star = styled.div`
-  position: relative;
+const StarPath = (props) => <BaseStarPath
+  fill="currentColor"
+  fillRule="evenodd"
+
+  d="M12 17.5l-4.672 2.456a1 1 0 0 1-1.451-1.054l.892-5.202-3.78-3.685a1 1 0 0 1 .555-1.706l5.223-.759 2.336-4.733a1 1 0 0 1 1.794 0l2.336 4.733 5.223.76a1 1 0 0 1 .555 1.705L17.23 13.7l.892 5.202a1 1 0 0 1-1.45 1.054L12 17.5z"
+  {...props}
+/>;
+
+const StarFillPath = styled(StarPath)`
+  color: ${palette('filler')};
 `;
+
+const randHash = () => Math.random().toString(36).substring(7);
+const MaskedStar = ({ value, ...props }) => {
+  const id = `star-mask-${randHash()}`;
+  const x = Math.round(((value % 1) * 0.8 + 0.1 ) * 24);
+  return (
+    <>
+      <mask id={id}>
+        <rect x="0" y="0" width="24" height="24" fill="white"  />
+        <rect x={x} y="0" width="24" height="24" fill="black"  />
+      </mask>
+      <StarPath mask={`url(#${id})`} {...props} />
+    </>
+  );
+};
 
 const Rating = ({
   palette, value, size, ...props
 }) => {
-  // TODO: fix hardcoded stroke size in svg
-  const stars = times(5, i => (
-    <Star key={`star${i}`}>
-      <BaseIcon
-        icon="star"
-        size={size}
-        palette={palette}
-      />
-      <PositionedMask palette={palette} width={getWidth(i, value)}>
-        <Icon
-          icon="star"
-          size={size}
-        />
-      </PositionedMask>
-    </Star>
-  ));
-
-  return <Wrapper {...props}>{stars}</Wrapper>;
+  return (
+    <>
+      <Wrapper {...props}>
+        <StyledStar size={size} viewBox="0 0 120 24">
+          {times(5, i => (
+            <Fragment key={`star${i}`}>
+              {value >= i + 1 && <StarPath palette={palette} transform={`translate(${i*24}, 0)`} /> }
+              {value < i + 1 && <StarFillPath palette={palette} transform={`translate(${i*24}, 0)`} /> }
+              {value > i && value < i + 1 && <MaskedStar palette={palette} value={value} transform={`translate(${i*24}, 0)`} /> }
+            </Fragment>
+              ))}
+        </StyledStar>
+      </Wrapper>
+    </>
+  );
 };
 
 Rating.propTypes = {
--- src/components/molecules/Rating/index.test.js
+++ src/components/molecules/Rating/index.test.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
-import Icon from 'sly/components/atoms/Icon';
-import Rating, { PositionedMask } from 'sly/components/molecules/Rating';
+import Rating from 'sly/components/molecules/Rating';
 
-const wrap = (props = {}) => shallow(<Rating {...props} />);
+const wrap = (props = {}) => mount(<Rating {...props} />);
 
 describe('Rating', () => {
   it('should render ratings', () => {
-    const wrapper = wrap({ value: 3.5 });
+    const wrapper = wrap({ value: 3.3 });
 
-    const icons = wrapper.find(Icon);
-    expect(icons).toHaveLength(5);
-    const masks = wrapper.find(PositionedMask);
-    expect(masks).toHaveLength(5);
+    const icon = wrapper.find('svg');
+    expect(icon.find('path')).toHaveLength(6);
 
-    masks.forEach((mask, i) => {
-      let expected = 0;
-      if (i < 3) {
-        expected = 100;
-      } else if (i < 4) {
-        expected = 50;
-      }
-      expect(mask.prop('width')).toEqual(expected);
-    });
+    const  mask = icon.find('mask');
+    expect(mask).toHaveLength(1);
+    expect(mask.find('rect')).toHaveLength(2);
+    expect(mask.find('rect').at(0).prop('fill')).toBe('white');
+    expect(mask.find('rect').at(1).prop('x')).toBe(8);
+    expect(mask.find('rect').at(1).prop('fill')).toBe('black');
   });
 });
 
