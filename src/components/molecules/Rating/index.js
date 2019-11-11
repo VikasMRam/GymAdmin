848d07e8a3ca40991768b84bde70a19f883a8050
Ed O'Reilly <ed@seniorly.com>
2019-11-11 18:35:17 +0000

    Fixing lint

--- src/components/molecules/Rating/index.js
+++ src/components/molecules/Rating/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-operators */
 import React, { Fragment } from 'react';
 import { number, string, oneOf } from 'prop-types';
 import styled from 'styled-components';
@@ -22,47 +23,52 @@ const BaseStarPath = styled.path`
   color: ${palette('base')};
 `;
 
-const StarPath = (props) => <BaseStarPath
-  fill="currentColor"
-  fillRule="evenodd"
-
-  d="M12 17.5l-4.672 2.456a1 1 0 0 1-1.451-1.054l.892-5.202-3.78-3.685a1 1 0 0 1 .555-1.706l5.223-.759 2.336-4.733a1 1 0 0 1 1.794 0l2.336 4.733 5.223.76a1 1 0 0 1 .555 1.705L17.23 13.7l.892 5.202a1 1 0 0 1-1.45 1.054L12 17.5z"
-  {...props}
-/>;
+const StarPath = props => (
+  <BaseStarPath
+    fill="currentColor"
+    fillRule="evenodd"
+    d="M12 17.5l-4.672 2.456a1 1 0 0 1-1.451-1.054l.892-5.202-3.78-3.685a1 1 0 0 1 .555-1.706l5.223-.759 2.336-4.733a1 1 0 0 1 1.794 0l2.336 4.733 5.223.76a1 1 0 0 1 .555 1.705L17.23 13.7l.892 5.202a1 1 0 0 1-1.45 1.054L12 17.5z"
+    {...props}
+  />
+);
 
 const StarFillPath = styled(StarPath)`
   color: ${palette('filler')};
 `;
 
-const randHash = () => Math.random().toString(36).substring(7);
+const randHash = () =>
+  Math.random()
+    .toString(36)
+    .substring(7);
+
+// eslint-disable-next-line react/prop-types
 const MaskedStar = ({ value, ...props }) => {
   const id = `star-mask-${randHash()}`;
-  const x = Math.round(((value % 1) * 0.8 + 0.1 ) * 24);
+  const x = Math.round(((value % 1) * 0.8 + 0.1) * 24);
   return (
     <>
       <mask id={id}>
-        <rect x="0" y="0" width="24" height="24" fill="white"  />
-        <rect x={x} y="0" width="24" height="24" fill="black"  />
+        <rect x="0" y="0" width="24" height="24" fill="white" />
+        <rect x={x} y="0" width="24" height="24" fill="black" />
       </mask>
       <StarPath mask={`url(#${id})`} {...props} />
     </>
   );
 };
 
-const Rating = ({
-  palette, value, size, ...props
-}) => {
+const Rating = ({ palette, value, size, ...props }) => {
   return (
     <>
       <Wrapper {...props}>
         <StyledStar size={size} viewBox="0 0 120 24">
           {times(5, i => (
             <Fragment key={`star${i}`}>
-              {value >= i + 1 && <StarPath palette={palette} transform={`translate(${i*24}, 0)`} /> }
-              {value < i + 1 && <StarFillPath palette={palette} transform={`translate(${i*24}, 0)`} /> }
-              {value > i && value < i + 1 && <MaskedStar palette={palette} value={value} transform={`translate(${i*24}, 0)`} /> }
+              {value >= i + 1 && <StarPath palette={palette} transform={`translate(${i * 24}, 0)`} />}
+              {value < i + 1 && <StarFillPath palette={palette} transform={`translate(${i * 24}, 0)`} />}
+              {value > i &&
+                value < i + 1 && <MaskedStar palette={palette} value={value} transform={`translate(${i * 24}, 0)`} />}
             </Fragment>
-              ))}
+          ))}
         </StyledStar>
       </Wrapper>
     </>
