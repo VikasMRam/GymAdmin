79504c09234ef51c17925a258f95baeb94e21fa6
Pranesh <pranesh@seniorly.com>
2019-11-12 12:21:15 +0530

    Removing store references related to searchBox

--- src/store/searchBox/selectors.js
+++
@@ -1,7 +0,0 @@
-export const initialState = {
-  address: '',
-  location: null,
-};
-
-export const searchBoxAddress = (state = initialState) => state.address;
-export const searchBoxLocation = (state = initialState) => state.location;
