5baa9766abd2056a20a4d51081894815822e90ec
Ed O'Reilly <ed@seniorly.com>
2019-11-20 16:13:15 +0000

    Explicitly removing redux-logger from the bundle when it's built in for prod

--- src/store/configure.js
+++ src/store/configure.js
@@ -2,31 +2,30 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { middleware as sagaThunkMiddleware } from 'redux-saga-thunk';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 
 import { middleware } from '../services/newApi';
 import reducer from './reducer';
 import sagas from './sagas';
 
-import { isDev, isBrowser, muteReduxLogger } from 'sly/config';
+import { isDev, isBrowser } from 'sly/config';
 
 const devtools =
   isDev && isBrowser && window.devToolsExtension
     ? window.devToolsExtension
     : () => fn => fn;
 
-const loggerMiddleware = createLogger();
+
+const optionalMiddleware = [];
+if(process.env.NODE_ENV !== 'production' && isBrowser) {
+  optionalMiddleware.push(require('redux-logger').createLogger());
+}
 
 export default function (initialState, services = {}) {
   const sagaMiddleware = createSagaMiddleware();
 
   const middlewares = [
-    middleware, thunkMiddleware, sagaThunkMiddleware, sagaMiddleware,
+    middleware, thunkMiddleware, sagaThunkMiddleware, sagaMiddleware, ...optionalMiddleware
   ];
-
-  if (isBrowser && !muteReduxLogger) {
-    middlewares.push(loggerMiddleware);
-  }
 
   const enhancers = [
     applyMiddleware(...middlewares),
