ac0dfc371413d43e3a1e4c4e893ed60fc15acff7
Amal <amal@seniorly.com>
2019-11-22 17:22:44 +0530

    fix /agents page form issues

--- src/components/organisms/TalkToAgentForm/index.js
+++ src/components/organisms/TalkToAgentForm/index.js
@@ -5,32 +5,22 @@ import { Field } from 'redux-form';
 import { ifProp } from 'styled-tools';
 
 import { size } from 'sly/components/themes';
+import userPropType from 'sly/propTypes/user';
 import { phoneParser, phoneFormatter } from 'sly/services/helpers/phone';
+import pad from 'sly/components/helpers/pad';
+import textAlign from 'sly/components/helpers/textAlign';
 import { Button, Block } from 'sly/components/atoms';
+import TosAndPrivacy from 'sly/components/molecules/TosAndPrivacy';
 import ReduxField from 'sly/components/organisms/ReduxField';
-import SearchBoxContainer from 'sly/containers/SearchBoxContainer';
 
-const StyledBlock = styled(Block)`
-  text-align: center;
-  margin-bottom: ${size('spacing.xLarge')};
-`;
+const StyledBlock = textAlign(pad(Block));
 
 const StyledButton = styled(Button)`
   margin-bottom: ${ifProp('error', size('spacing.large'), 0)};
   width: 100%;
 `;
 
-const StyledSearchBoxContainer = styled(SearchBoxContainer)`
-  margin-bottom: ${size('spacing.large')};
-`;
-
-const Label = styled(Block)`
-  margin-bottom: ${size('spacing.small')};
-`;
-
-const noRender = () => null;
-
-class TalkToAgentForm extends Component {
+export default class TalkToAgentForm extends Component {
   static propTypes = {
     handleSubmit: func.isRequired,
     invalid: bool,
@@ -38,75 +28,88 @@ class TalkToAgentForm extends Component {
     error: string,
     change: func,
     onLocationChange: func,
-    heading: string,
+    heading: string.isRequired,
+    user: userPropType,
+    hasLocation: bool,
+    hasEmail: bool,
+    firstName: string.isRequired,
   };
 
   static defaultProps = {
     heading: 'Talk to a local Seniorly Agent',
-  };
-
-  handleChange = () => {
-    const { change } = this.props;
-    change('location', null);
-  };
-
-  handleLocationChange = (value) => {
-    const { change, onLocationChange } = this.props;
-    change('location', value);
-    if (onLocationChange) {
-      onLocationChange(value);
-    }
+    firstName: 'we',
   };
 
   render() {
-    const { handleChange, handleLocationChange } = this;
     const {
-      invalid, submitting, handleSubmit, error, heading,
+      invalid, submitting, handleSubmit, error, heading, user, hasLocation, hasEmail,
+      firstName,
     } = this.props;
+
+    const showTos = !!user;
 
     return (
       <section>
         <StyledBlock size="title" weight="medium">{heading}</StyledBlock>
         <form onSubmit={handleSubmit}>
-          <Label size="caption">Where are you searching for homes? *</Label>
-          <StyledSearchBoxContainer
-            clearLocationOnBlur={false}
-            onLocationSearch={handleLocationChange}
-            onTextChange={handleChange}
-          />
-          <Field name="location" component={noRender} />
-          <Field
-            name="name"
-            label="Full Name *"
-            type="text"
-            placeholder="Full Name"
-            component={ReduxField}
-          />
-          <Field
-            name="phone"
-            label="Phone *"
-            type="text"
-            parse={phoneParser}
-            format={phoneFormatter}
-            placeholder="925-555-5555"
-            component={ReduxField}
-          />
+          {hasLocation &&
+            <Field
+              name="location"
+              label="Where are you searching for homes?"
+              type="locationSearch"
+              placeholder="Search By City, State"
+              component={ReduxField}
+              required
+            />
+          }
+          {!(user && user.name) &&
+            <Field
+              name="name"
+              label="Full Name"
+              type="text"
+              placeholder="Full Name"
+              component={ReduxField}
+              required
+            />
+          }
+          {!(user && user.email) && hasEmail &&
+            <Field
+              name="email"
+              label="Email"
+              type="email"
+              placeholder="Email"
+              component={ReduxField}
+              required
+            />
+          }
+          {!(user && user.phoneNumber) &&
+            <Field
+              name="phone"
+              label="Phone"
+              type="text"
+              parse={phoneParser}
+              format={phoneFormatter}
+              placeholder="925-555-5555"
+              component={ReduxField}
+              required
+            />
+          }
           <Field
             type="textarea"
             rows="3"
             name="message"
-            label="What can we help you with? *"
+            label={`What can ${firstName} help you with?`}
             placeholder="I'm interested in a free consult with a Seniorly Agent."
             component={ReduxField}
+            required
           />
-          <StyledButton error={error} type="submit" kind="jumbo" disabled={invalid || submitting}>
+          <StyledButton error={error || showTos} type="submit" kind="jumbo" disabled={invalid || submitting}>
             Send
           </StyledButton>
+          {showTos && <TosAndPrivacy />}
         </form>
         {error && <Block palette="danger">{error}</Block>}
       </section>
     );
   }
 }
-
-export default TalkToAgentForm;
