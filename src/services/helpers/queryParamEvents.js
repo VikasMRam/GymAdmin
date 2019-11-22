21ec0a7fc4e8d267b3ddd42e242e2504b9780ac0
Ed O'Reilly <ed@seniorly.com>
2019-11-22 09:45:18 +0000

    Adding helpers for adding and extracting events from the query string

---
+++ src/services/helpers/queryParamEvents.js
@@ -0,0 +1,68 @@
+import { URL, parse as parseUrl } from 'url';
+import { stringify, parse } from 'query-string';
+
+const propToQueryParam = {
+  action: 'sly_action' ,
+  label: 'sly_label',
+  category: 'sly_category',
+  value: 'sly_value',
+  nonInteraction: 'sly_ni',
+};
+
+export function extractEventFromQuery(search) {
+  const qs = parse(search);
+  if (!qs.sly_action) {
+    return { event: null, search };
+  }
+
+  const {
+    sly_action: action,
+    sly_label: label,
+    sly_category: category,
+    sly_value: value,
+    sly_ni: nonInteraction,
+    ...rest
+  } = qs;
+  const remainingSearch = stringify(rest, {
+    sort: (a, b) => search.indexOf(a) - search.indexOf(b),
+  });
+
+  return {
+    event: Object.assign(
+      { action },
+      typeof label !== 'undefined' && { label },
+      typeof category !== 'undefined' && { category },
+      typeof value !== 'undefined' && { value: Number(value) || value },
+      typeof nonInteraction !== 'undefined' && { nonInteraction: nonInteraction === 'true' }
+    ),
+    search: remainingSearch.length > 1 ? `?${remainingSearch}` : '',
+  };
+}
+
+export function addEventToQueryString(event, search) {
+  if (!event || !event.action) {
+    return search;
+  }
+
+
+  const serializedEvent = Object.keys(event).reduce((acc, key) => {
+    if(event[key] !== 'undefined') {
+      acc.push(`${propToQueryParam[key]}=${event[key]}`);
+    }
+
+    return acc;
+  }, []).join('&');
+
+  const maybeQuestion = search[0] === '?' ? '' : '?';
+  const maybeAnd = search.length === 0 ? '' : '&';
+
+  return maybeQuestion+search+maybeAnd+serializedEvent;
+}
+
+export function addEventToUrl(event, urlString) {
+  const url = parseUrl(urlString);
+
+  url.search = addEventToQueryString(event, url.search || '');
+
+  return url.format();
+}
