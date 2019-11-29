2ede4e73460b44357d0a6ec6f595983f2b805f7d
Fonz <fonz@botverse.com>
2019-11-29 13:39:49 +0000

    removed react-router-server

--- src/services/partialHydration/partiallyHydrateClient.js
+++ src/services/partialHydration/partiallyHydrateClient.js
@@ -7,7 +7,6 @@ import 'react-hot-loader/patch';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { ServerStateProvider } from 'react-router-server';
 import Modal from 'react-modal';
 import { ThemeProvider } from 'styled-components';
 import Route from 'react-router/Route';
@@ -18,7 +17,6 @@ import theme from 'sly/components/themes/default';
 import { hydrateComponents } from 'sly/services/partialHydration/index';
 
 export default function partiallyHydrateClient(componentsToHydrate, routePath, root) {
-  const serverState = window.__SERVER_STATE__;
   const initialState = window.__INITIAL_STATE__;
   const store = configureStore(initialState);
 
@@ -28,17 +26,15 @@ export default function partiallyHydrateClient(componentsToHydrate, routePath, r
 
   // eslint-disable-next-line react/prop-types
   const Providers = ({ children }) => (
-    <ServerStateProvider state={serverState}>
-      <ApiProvider api={beesApi}>
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <BrowserRouter>
-              <Route path={routePath} render={() => children} />
-            </BrowserRouter>
-          </ThemeProvider>
-        </Provider>
-      </ApiProvider>
-    </ServerStateProvider>
+    <ApiProvider api={beesApi}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Route path={routePath} render={() => children} />
+          </BrowserRouter>
+        </ThemeProvider>
+      </Provider>
+    </ApiProvider>
   );
 
   hydrateComponents(componentsToHydrate, root, Providers);
