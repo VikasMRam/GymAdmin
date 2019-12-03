d239dcb3b2cd402b9a86a01365379638baf7c380
Fonz <fonz@botverse.com>
2019-12-03 13:34:11 +0000

    fix WizardController

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,6 +8,10 @@ import { set, unset, reset } from 'sly/store/controller/actions';
 
 // TODO: tests
 export function connectController(parentMapStateToProps, parentDispatchToProps) {
+  if (typeof parentDispatchToProps === 'function' && parentDispatchToProps.length > 1) {
+    throw new Error('dispatchToProps should not use ownProps');
+  }
+
   return function controllerCreator(WrappedComponent) {
     const Controller = props => <WrappedComponent {...props} />;
     Controller.displayName = `WrappedController(${WrappedComponent.name || 'Controller'})`;
@@ -31,16 +35,14 @@ export function connectController(parentMapStateToProps, parentDispatchToProps) 
       };
     };
 
-    const ConnectedController = connect(mapStateToProps, mapDispatchToProps)(props => {
-      return (
-        <Controller
-          {...props}
-          set={data => props.set({ data, controller: props.controllerKey })}
-          unset={key => props.unset({ key, controller: props.controllerKey })}
-          resetController={() => props.reset({ controller: props.controllerKey })}
-        />
-      )
-    });
+    const ConnectedController = connect(mapStateToProps, mapDispatchToProps)(props => (
+      <Controller
+        {...props}
+        set={data => props.set({ data, controller: props.controllerKey })}
+        unset={key => props.unset({ key, controller: props.controllerKey })}
+        resetController={() => props.reset({ controller: props.controllerKey })}
+      />
+    ));
 
     hoistNonReactStatic(ConnectedController, WrappedComponent);
 
