1b13eebb101c50ff01ad4700ac4d02d98650a18a
Fonz <fonz@botverse.com>
2019-12-05 11:41:55 +0000

    search page runtime performance

--- src/services/helpers/hocs.js
+++ src/services/helpers/hocs.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import hoistStatics from 'hoist-non-react-statics';
+import memoize from 'memoize-one';
+import isDeepEqual from 'lodash/isEqual';
 
 export const withProps = (digestProps = _ => _) => {
+  const memoizedDigest = memoize(digestProps, isDeepEqual);
   return (ChildComponent) => {
     const Wrapper = props => (
       <ChildComponent
         {...{
           ...props,
-          ...digestProps(props),
+          ...memoizedDigest(props),
         }}
       />
     );
