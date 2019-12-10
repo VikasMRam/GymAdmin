18d9a2a8715ed5d396d14abca9f0291c5b8ddc43
Fonz <fonz@botverse.com>
2019-12-10 16:26:42 +0000

    lint

--- src/components/themes/DatepickerStyles.js
+++ src/components/themes/DatepickerStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 let css;
 try {
-  // eslint-disable-next-line import/no-webpack-loader-syntax
+  // eslint-disable-next-line import/no-webpack-loader-syntax,import/no-unresolved
   css = require('!raw-loader!./react-datepicker.css');
 } catch (e) {
   // eslint-disable-next-line no-console
