f3200245b6d00699b6f3330abc5809ccb8ee4cb8
Fonz <fonz@botverse.com>
2019-12-10 11:52:33 +0000

    added usedExports optimization

--- webpack.config.js
+++ webpack.config.js
@@ -302,6 +302,7 @@ const client = (target, entries) => {
     when(isWeb, [
       optimization({
         // concatenateModules: false,
+        usedExports: true,
         splitChunks: {
           chunks: 'all',
         },
