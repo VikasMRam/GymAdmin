47a8c9614846ed683ace3ff2d12e34f09e83efdc
Pranesh <pranesh@seniorly.com>
2019-12-31 14:00:29 +0530

    Using conversation name if returned from api

--- src/services/helpers/conversation.js
+++ src/services/helpers/conversation.js
@@ -1,20 +1,19 @@
 import { CONVERSATION_PARTICIPANT_TYPE_ORGANIZATION } from 'sly/constants/conversations';
-import { titleize } from 'sly/services/helpers/strings';
 
 export const getConversationName = (conversation, user) => {
-  const { conversationParticipants } = conversation;
+  const { conversationParticipants, info } = conversation;
+  if (info.Name) {
+    return info.Name;
+  }
   const { id: userId } = user;
   const nameList = conversationParticipants
     .filter(conversationParticipant => conversationParticipant.participantID !== userId)
     .filter(conversationParticipant => conversationParticipant.participantType !== CONVERSATION_PARTICIPANT_TYPE_ORGANIZATION)
     .reduce((accumulator, conversationParticipant) => {
       const { participantInfo } = conversationParticipant;
-      const { name, roleName } = participantInfo;
-      if (name !== '' && roleName !== 'slyUser') {
-        const result = `[${titleize(roleName)}]: ${name}`;
-        if (accumulator.indexOf(name) === -1) {
-          accumulator.push(result);
-        }
+      const { name } = participantInfo;
+      if (name !== '' && accumulator.indexOf(name) === -1) {
+        accumulator.push(name);
       }
       return accumulator;
     }, []);
