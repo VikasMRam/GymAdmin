5e4f78a7ef791fe414d30bacbf0f0c1120515a16
Fonz <fonz@botverse.com>
2020-01-03 17:47:09 +0000

    some lint

--- image-handler-lambda/ImageHandlerFunction/index.js
+++ image-handler-lambda/ImageHandlerFunction/index.js
@@ -1,4 +1,4 @@
-/*********************************************************************************************************************
+/** *******************************************************************************************************************
  *  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
  *                                                                                                                    *
  *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
@@ -9,38 +9,38 @@
  *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
  *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
  *  and limitations under the License.                                                                                *
- *********************************************************************************************************************/
+ ******************************************************************************************************************** */
 
 const ImageRequest = require('./image-request.js');
 const ImageHandler = require('./image-handler.js');
 
 exports.handler = async (event) => {
-    const imageRequest = new ImageRequest();
-    const imageHandler = new ImageHandler();
-    try {
-        const request = await imageRequest.setup(event, process.env.BUCKET);
-        // console.log(request);
-        const processedRequest = await imageHandler.process(request);
+  const imageRequest = new ImageRequest();
+  const imageHandler = new ImageHandler();
+  try {
+    const request = await imageRequest.setup(event, process.env.BUCKET);
+    // console.log(request);
+    const processedRequest = await imageHandler.process(request);
 
-        await imageRequest.uploadEditedImage(processedRequest);
+    await imageRequest.uploadEditedImage(processedRequest);
 
-        return response = {
-            "statusCode": 200,
-            "headers" : getResponseHeaders(request.contentType),
-            "body": processedRequest.toString('base64'),
-            "isBase64Encoded": true
-        };
-    } catch (err) {
-        console.log(err);
-        const response = {
-            "statusCode": err.status,
-            "headers" : getResponseHeaders('application/json'),
-            "body": JSON.stringify(err),
-            "isBase64Encoded": false
-        };
-        return response;
-    }
-}
+    return response = {
+      statusCode: 200,
+      headers: getResponseHeaders(request.contentType),
+      body: processedRequest.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (err) {
+    console.log(err);
+    const response = {
+      statusCode: err.status,
+      headers: getResponseHeaders('application/json'),
+      body: JSON.stringify(err),
+      isBase64Encoded: false,
+    };
+    return response;
+  }
+};
 
 /**
  * Generates the appropriate set of response headers based on a success
@@ -48,15 +48,15 @@ exports.handler = async (event) => {
  * @param {boolean} isErr - has an error been thrown?
  */
 const getResponseHeaders = (contentType) => {
-    const corsEnabled = (process.env.CORS_ENABLED === "Yes");
-    const headers = {
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Credentials": true,
-        "Content-Type": contentType,
-    }
-    if (corsEnabled) {
-        headers["Access-Control-Allow-Origin"] = process.env.CORS_ORIGIN;
-    }
-    return headers;
+  const corsEnabled = (process.env.CORS_ENABLED === 'Yes');
+  const headers = {
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Credentials': true,
+    'Content-Type': contentType,
+  };
+  if (corsEnabled) {
+    headers['Access-Control-Allow-Origin'] = process.env.CORS_ORIGIN;
+  }
+  return headers;
 };
