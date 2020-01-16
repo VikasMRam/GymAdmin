c834e1e3038e98efaf4475c17a9a1ef3ae438f3a
Pranesh <pranesh@seniorly.com>
2020-01-16 12:49:02 +0530

    Checking for changes every second in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -167,6 +167,11 @@ const base = group([
     writeToDisk(filePath) {
       return /dist\/(node\/|server)/.test(filePath) || /loadable-stats/.test(filePath);
     },
+    watchOptions: {
+      poll: 1000,
+      ignored: ['node_modules'],
+    },
+    hot: true,
   }),
 
   match(['*.js', '!*node_modules*'], [babel()]),
