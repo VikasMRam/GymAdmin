21871423404e10e618a0bbad36dc6340de377dae
Pranesh <pranesh@seniorly.com>
2020-01-17 22:23:24 +0530

    Added some more fields for Agent Account form

--- src/components/organisms/PartnerAgentAccountForm/index.js
+++ src/components/organisms/PartnerAgentAccountForm/index.js
@@ -3,9 +3,42 @@ import { Field } from 'redux-form';
 
 import ReduxField from 'sly/components/organisms/ReduxField';
 import FormSection from 'sly/components/molecules/FormSection';
+import { phoneParser, phoneFormatter } from 'sly/services/helpers/phone';
 
 const PartnerAgentAccountForm = ({ ...props }) => (
   <FormSection heading="Edit Account" buttonText="Save" {...props}>
+    <Field
+      name="region"
+      label="Region"
+      type="text"
+      placeholder=""
+      component={ReduxField}
+      wideWidth
+    />
+    <Field
+      name="imageCaption"
+      label="Referral Agent Image Caption"
+      type="text"
+      placeholder=""
+      component={ReduxField}
+      wideWidth
+    />
+    <Field
+      name="cv"
+      label="Referral Agent Free Form"
+      type="textarea"
+      placeholder=""
+      component={ReduxField}
+      wideWidth
+    />
+    <Field
+      name="bio"
+      label="Bio"
+      type="textarea"
+      placeholder=""
+      component={ReduxField}
+      wideWidth
+    />
     <Field
       name="displayName"
       label="Display name"
@@ -15,9 +48,26 @@ const PartnerAgentAccountForm = ({ ...props }) => (
       wideWidth
     />
     <Field
-      name="bio"
-      label="Bio"
+      name="chosenReview"
+      label="RA Chosen Review"
       type="textarea"
+      placeholder=""
+      component={ReduxField}
+      wideWidth
+    />
+    <Field
+      name="slyPhone"
+      label="RA Twilio Phone"
+      placeholder=""
+      parse={phoneParser}
+      format={phoneFormatter}
+      component={ReduxField}
+      wideWidth
+    />
+    <Field
+      name="vacation"
+      label="RA Vacation"
+      type="daterange"
       placeholder=""
       component={ReduxField}
       wideWidth
