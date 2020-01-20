db4fddaa3a502e0f26cfa41198f86a0fb48f678a
Fonz <fonz@botverse.com>
2020-01-20 12:11:56 +0000

    added assisted living communities

--- src/components/organisms/PostConversionGreeting/index.stories.js
+++ src/components/organisms/PostConversionGreeting/index.stories.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+import RhodaGoldmanPlaza from 'sly/../private/storybook/sample-data/property-rhoda-goldman-plaza.json';
 import PostConversionGreeting from 'sly/components/organisms/PostConversionGreeting';
 
 const onReject = action('onReject');
 
 storiesOf('Organisms|PostConversionGreeting', module)
   .add('default', () => (
-    <PostConversionGreeting onReject={onReject} />
+    <PostConversionGreeting onReject={onReject} community={RhodaGoldmanPlaza} />
   ));
