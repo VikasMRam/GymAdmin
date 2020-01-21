fadce558213f512ef8644ae68b2693ff5c01b041
Pranesh <pranesh@seniorly.com>
2020-01-21 15:34:19 +0530

    Adding all Sections of Agent Account form

--- src/components/organisms/PartnerAgentAccountForm/index.js
+++ src/components/organisms/PartnerAgentAccountForm/index.js
@@ -1,78 +1,156 @@
 import React from 'react';
 import { Field } from 'redux-form';
+import styled from 'styled-components';
 
+import { size } from 'sly/components/themes';
 import ReduxField from 'sly/components/organisms/ReduxField';
-import FormSection from 'sly/components/molecules/FormSection';
-import { phoneParser, phoneFormatter } from 'sly/services/helpers/phone';
+import HeadingBoxSection from 'sly/components/molecules/HeadingBoxSection';
+import { Block, Hr, Button } from 'sly/components/atoms';
+// import { phoneParser, phoneFormatter } from 'sly/services/helpers/phone';
 
-const PartnerAgentAccountForm = ({ ...props }) => (
-  <FormSection heading="Edit Account" buttonText="Save" {...props}>
-    <Field
-      name="region"
-      label="Region"
-      type="text"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="imageCaption"
-      label="Referral Agent Image Caption"
-      type="text"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="cv"
-      label="Referral Agent Free Form"
-      type="textarea"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="bio"
-      label="Bio"
-      type="textarea"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="displayName"
-      label="Display name"
-      type="text"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="chosenReview"
-      label="RA Chosen Review"
-      type="textarea"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="slyPhone"
-      label="RA Twilio Phone"
-      placeholder=""
-      parse={phoneParser}
-      format={phoneFormatter}
-      component={ReduxField}
-      wideWidth
-    />
-    <Field
-      name="vacation"
-      label="RA Vacation"
-      type="daterange"
-      placeholder=""
-      component={ReduxField}
-      wideWidth
-    />
-  </FormSection>
+const StyledForm = styled.form`
+  > section {
+    margin-bottom: ${size('spacing.xLarge')};
+  }
+`;
+
+const BottomWrapper = styled.div`
+  margin-left: -${size('spacing.xLarge')};
+  margin-right: -${size('spacing.xLarge')};
+  text-align: right;
+`;
+
+const BottomButton = styled(Button)`
+  margin-right: ${size('spacing.xLarge')};
+`;
+
+const AGENT_REGION_OPTIONS = [
+  'Pacific North',
+  'Pacific South',
+  'Central North',
+  'Central South',
+  'North East',
+  'South East',
+  'Florida',
+];
+const AGENT_STATUS_OPTIONS = [
+  { value: -2, label: 'Deleted' },
+  { value: -1, label: 'Pending' },
+  { value: 0, label: 'Approved' },
+  { value: 1, label: 'Live' },
+];
+
+const agentRegionOptions = AGENT_REGION_OPTIONS.map(i => <option key={i} value={i}>{i}</option>);
+const agentStatusOptions = AGENT_STATUS_OPTIONS.map(i => <option key={i.value} value={i.value}>{i.label}</option>);
+
+const PartnerAgentAccountForm = ({ buttonText, error, handleSubmit, pristine, submitting, invalid,hasNoBodyPadding }) => (
+  <StyledForm onSubmit={handleSubmit}>
+    <HeadingBoxSection heading="Area Served" hasNoBodyPadding={hasNoBodyPadding}>
+      <Field
+        name="adminRegion"
+        label="Region"
+        type="select"
+        component={ReduxField}
+        wideWidth
+      >
+        <option value="" disabled>Select an option</option>
+        {agentRegionOptions}
+      </Field>
+      <Field
+        name="zipcodesServed"
+        label="Zipcodes Served"
+        type="textarea"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+    </HeadingBoxSection>
+    <HeadingBoxSection heading="Profile" hasNoBodyPadding={hasNoBodyPadding}>
+      <Field
+        name="bio"
+        label="Bio"
+        type="textarea"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="parentCompany"
+        label="Parent Company"
+        type="text"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="displayName"
+        label="Display name"
+        type="text"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="cv"
+        label="Rich Text Bio"
+        type="textarea"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="imageCaption"
+        label="Image Caption"
+        type="text"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="chosenReview"
+        label="Chosen Review"
+        type="text"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+      <Field
+        name="vacation"
+        label="Vacation"
+        type="daterange"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+    </HeadingBoxSection>
+    <HeadingBoxSection heading="Admin" hasNoBodyPadding={hasNoBodyPadding}>
+      <Field
+        name="status"
+        label="Status"
+        type="select"
+        component={ReduxField}
+        wideWidth
+      >
+        <option value="" disabled>Select an option</option>
+        {agentStatusOptions}
+      </Field>
+      <Field
+        name="adminNotes"
+        label="Admin Notes"
+        type="textarea"
+        placeholder=""
+        component={ReduxField}
+        wideWidth
+      />
+    </HeadingBoxSection>
+    {error && <Block palette="danger">{error}</Block>}
+    {buttonText &&
+      <BottomWrapper>
+        <Hr />
+        <BottomButton type="submit" disabled={invalid || pristine || submitting}>{buttonText}</BottomButton>
+      </BottomWrapper>
+    }
+  </StyledForm>
 );
 
 export default PartnerAgentAccountForm;
--- src/containers/PartnerAgentAccountFormController.js
+++ src/containers/PartnerAgentAccountFormController.js
@@ -44,14 +44,25 @@ export default class PartnerAgentAccountFormController extends Component {
     } = this.props;
     const { agent: rawAgent } = status;
     console.log(rawAgent);
+    console.log(values);
     const { result } = rawAgent;
     const { id } = result;
 
-    const agent = immutable.wrap(pick(result, ['id', 'type', 'attributes.info']))
+    const agent = immutable.wrap(pick(result, ['id', 'type', 'attributes.status', 'attributes.info', 'attributes.adminInfo', 'attributes.info.serviceArea']))
       .set('attributes.info.bio', values.bio)
+      .set('attributes.info.parentCompany', values.parentCompany)
       .set('attributes.info.displayName', values.displayName)
+      .set('attributes.info.cv', values.cv)
+      .set('attributes.info.imageCaption', values.imageCaption)
+      .set('attributes.info.chosenReview', values.chosenReview)
+      .set('attributes.adminInfo.adminRegion', values.adminRegion)
+      .set('attributes.info.serviceArea.zipcodesServed', values.zipcodesServed)
+      .set('attributes.adminInfo.vacationStart', values.vacation[0])
+      .set('attributes.adminInfo.vacationEnd', values.vacation[1])
+      .set('attributes.status', parseInt(values.status, 10))
+      .set('attributes.adminInfo.adminNotes', values.adminNotes)
       .value();
-
+    console.log(agent);
     const agentPromise = () => updateAgent({ id }, agent);
 
     return agentPromise().then(notifySuccess('Details Updated Successfully'))
@@ -70,14 +81,18 @@ export default class PartnerAgentAccountFormController extends Component {
     const { hasFinished: agentHasFinished } = status.agent;
     if (agentHasFinished) {
       console.log(agent);
-      const { info } = agent;
-      const { bio, displayName } = info;
-      const initialValues = { bio, displayName };
+      const { info, adminInfo, status } = agent;
+      const { bio, parentCompany, displayName, cv, imageCaption, chosenReview, serviceArea } = info;
+      const { adminRegion, vacationStart, vacationEnd, adminNotes } = adminInfo;
+      const { zipcodesServed } = serviceArea;
+      const vacation = [new Date(vacationStart), new Date(vacationEnd)];
+      const initialValues = { bio, parentCompany, displayName, cv, imageCaption, chosenReview, adminRegion, zipcodesServed, vacation, status, adminNotes };
       return (
         <ReduxForm
           initialValues={initialValues}
           onSubmit={this.handleSubmit}
           user={user}
+          buttonText="Save"
           {...props}
         />
       );
