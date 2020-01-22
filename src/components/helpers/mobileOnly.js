e8007f8a618c186921613058215718e3a3e5ef1d
Fonz <fonz@botverse.com>
2020-01-22 15:46:42 +0000

    fixed filters fields width

--- src/components/helpers/mobileOnly.js
+++ src/components/helpers/mobileOnly.js
@@ -4,10 +4,10 @@ import { size } from 'sly/components/themes';
 
 const mobileOnly = (Component, css, otherwiseCss) => styled(Component)`
   @media screen and (max-width: calc(${size('breakpoint.tablet')} - 1px)) {
-    ${css}; 
+    ${css};
   }
   @media screen and (min-width: ${size('breakpoint.tablet')}) {
-    ${otherwiseCss && otherwiseCss}; 
+    ${otherwiseCss};
   }
 `;
 
