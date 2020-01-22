722581d72f4eb5fcf4a6ffbfd8c450f32ba6b0b6
Pranesh <pranesh@seniorly.com>
2020-01-22 18:17:17 +0530

    Using FamilyDetailsForm styles for PartnerAgentProfileForm

--- src/components/organisms/PartnerAgentProfileForm/index.js
+++ src/components/organisms/PartnerAgentProfileForm/index.js
@@ -2,27 +2,53 @@ import React from 'react';
 import { Field } from 'redux-form';
 import styled from 'styled-components';
 
-import { size } from 'sly/components/themes';
+import { size, palette } from 'sly/components/themes';
 import ReduxField from 'sly/components/organisms/ReduxField';
-import HeadingBoxSection from 'sly/components/molecules/HeadingBoxSection';
-import { Block, Hr, Button } from 'sly/components/atoms';
-// import { phoneParser, phoneFormatter } from 'sly/services/helpers/phone';
+import { Block, Button } from 'sly/components/atoms';
+import pad from 'sly/components/helpers/pad';
 
-const StyledForm = styled.form`
-  > section {
-    margin-bottom: ${size('spacing.xLarge')};
+const StyledButton = pad(Button, 'regular');
+StyledButton.displayName = 'StyledButton';
+
+const Form = styled.form`
+  background-color: ${palette('white.base')};
+`;
+Form.displayName = 'Form';
+
+const Warning = pad(styled(Block)`
+  background-color: ${palette('warning.filler')};
+  border-radius: ${size('border.xxLarge')};
+  text-align: center;
+  padding: ${size('spacing.large')};
+`, 'xLarge');
+Warning.displayName = 'Warning';
+
+const FormScrollSection = styled.div`
+  max-height: calc(100vh - 240px);
+  overflow-y: auto;
+`;
+
+const FormSection = styled.div`
+  padding: ${size('spacing.xLarge')} ${size('spacing.large')};
+  padding-bottom: 0;
+  border-bottom: ${size('border.regular')} solid ${palette('slate', 'stroke')};
+
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    padding: ${size('spacing.xLarge')};
+    padding-bottom: 0;
+  }
+`;
+const FormBottomSection = styled.div`
+  padding: ${size('spacing.xLarge')} ${size('spacing.large')};
+  border-bottom: ${size('border.regular')} solid ${palette('slate', 'stroke')};
+  box-shadow: 0 ${size('spacing.small')} ${size('spacing.regular')} ${palette('grey', 'filler')};
+
+  @media screen and (min-width: ${size('breakpoint.tablet')}) {
+    padding: ${size('spacing.xLarge')};
   }
 `;
 
-const BottomWrapper = styled.div`
-  margin-left: -${size('spacing.xLarge')};
-  margin-right: -${size('spacing.xLarge')};
-  text-align: right;
-`;
-
-const BottomButton = styled(Button)`
-  margin-right: ${size('spacing.xLarge')};
-`;
+const FormSectionHeading = pad(Block, 'large');
 
 const AGENT_REGION_OPTIONS = [
   'Pacific North',
@@ -44,121 +70,127 @@ const agentRegionOptions = AGENT_REGION_OPTIONS.map(i => <option key={i} value={
 const agentStatusOptions = AGENT_STATUS_OPTIONS.map(i => <option key={i.value} value={i.value}>{i.label}</option>);
 
 const PartnerAgentProfileForm = ({ buttonText, error, handleSubmit, pristine, submitting, invalid,hasNoBodyPadding }) => (
-  <StyledForm onSubmit={handleSubmit}>
-    <HeadingBoxSection heading="Area Served" hasNoBodyPadding={hasNoBodyPadding}>
-      <Field
-        name="adminRegion"
-        label="Region"
-        type="select"
-        component={ReduxField}
-        wideWidth
-      >
-        <option value="" disabled>Select an option</option>
-        {agentRegionOptions}
-      </Field>
-      <Field
-        name="zipcodesServed"
-        label="Zipcodes Served"
-        type="textarea"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-    </HeadingBoxSection>
-    <HeadingBoxSection heading="Profile" hasNoBodyPadding={hasNoBodyPadding}>
-      <Field
-        name="bio"
-        label="Bio"
-        type="textarea"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="parentCompany"
-        label="Parent Company"
-        type="text"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="displayName"
-        label="Display name"
-        type="text"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="cv"
-        label="Rich Text Bio"
-        type="textarea"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="imageCaption"
-        label="Image Caption"
-        type="text"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="chosenReview"
-        label="Chosen Review"
-        type="text"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="vacation"
-        label="Vacation"
-        type="daterange"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-    </HeadingBoxSection>
-    <HeadingBoxSection heading="Admin" hasNoBodyPadding={hasNoBodyPadding}>
-      <Field
-        name="status"
-        label="Status"
-        type="select"
-        component={ReduxField}
-        wideWidth
-      >
-        <option value="" disabled>Select an option</option>
-        {agentStatusOptions}
-      </Field>
-      <Field
-        name="adminNotes"
-        label="Admin Notes"
-        type="textarea"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-      <Field
-        name="slyScore"
-        label="Slyscore"
-        type="number"
-        placeholder=""
-        component={ReduxField}
-        wideWidth
-      />
-    </HeadingBoxSection>
+  <Form onSubmit={handleSubmit}>
+    <FormScrollSection>
+      <FormSection>
+        <FormSectionHeading weight="medium">Area Served</FormSectionHeading>
+        <Field
+          name="adminRegion"
+          label="Region"
+          type="select"
+          component={ReduxField}
+          wideWidth
+        >
+          <option value="" disabled>Select an option</option>
+          {agentRegionOptions}
+        </Field>
+        <Field
+          name="zipcodesServed"
+          label="Zipcodes Served"
+          type="textarea"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+      </FormSection>
+      <FormSection>
+        <FormSectionHeading weight="medium">Profile</FormSectionHeading>
+        <Field
+          name="bio"
+          label="Bio"
+          type="textarea"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="parentCompany"
+          label="Parent Company"
+          type="text"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="displayName"
+          label="Display name"
+          type="text"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="cv"
+          label="Rich Text Bio"
+          type="textarea"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="imageCaption"
+          label="Image Caption"
+          type="text"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="chosenReview"
+          label="Chosen Review"
+          type="text"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="vacation"
+          label="Vacation"
+          type="daterange"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+      </FormSection>
+      <FormSection>
+        <FormSectionHeading weight="medium">Admin</FormSectionHeading>
+        <Field
+          name="status"
+          label="Status"
+          type="select"
+          component={ReduxField}
+          wideWidth
+        >
+          <option value="" disabled>Select an option</option>
+          {agentStatusOptions}
+        </Field>
+        <Field
+          name="adminNotes"
+          label="Admin Notes"
+          type="textarea"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+        <Field
+          name="slyScore"
+          label="Slyscore"
+          type="number"
+          placeholder=""
+          component={ReduxField}
+          wideWidth
+        />
+      </FormSection>
+    </FormScrollSection>
     {error && <Block palette="danger">{error}</Block>}
     {buttonText &&
-      <BottomWrapper>
-        <Hr />
-        <BottomButton type="submit" disabled={invalid || pristine || submitting}>{buttonText}</BottomButton>
-      </BottomWrapper>
+      <FormBottomSection>
+        <StyledButton type="submit" disabled={invalid || pristine || submitting}>
+          {buttonText}
+        </StyledButton>
+      </FormBottomSection>
     }
-  </StyledForm>
+  </Form>
 );
 
 export default PartnerAgentProfileForm;
