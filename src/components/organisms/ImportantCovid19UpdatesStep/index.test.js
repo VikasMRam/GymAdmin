da892eaf78a6e806193e242f959c6014960032bd
Amal Francis <amal@seniorly.com>
2020-05-04 18:57:16 +0530

    update ImportantCovid19UpdatesStep tests

--- src/components/organisms/ImportantCovid19UpdatesStep/index.test.js
+++ src/components/organisms/ImportantCovid19UpdatesStep/index.test.js
@@ -5,11 +5,12 @@ import ImportantCovid19UpdatesStep from 'sly/components/organisms/ImportantCovid
 
 const buttons = [
   {
-    text: 'Get information about in-home caregivers if you are interested in delaying your move',
-    props: {
-      onClick: jest.fn(),
-      ghost: true,
-    },
+    label: 'Get information about senior living communities that are currently accepting new residents',
+    value: 'admission-info',
+  },
+  {
+    label: 'Get information about in-home caregivers if you are interested in delaying your move',
+    value: 'in-home',
   },
 ];
 const defaultProps = {
@@ -26,23 +27,23 @@ describe('ImportantCovid19UpdatesStep', () => {
   it('renders', () => {
     const cityName = 'cityName';
     const wrapper = wrap({ cityName });
-    const renderedButtons = wrapper.find('Button');
+    const renderedButtons = wrapper.find('Field');
 
     expect(wrapper.find('TextAlignCenterHeading').text()).toContain(cityName);
     expect(renderedButtons).toHaveLength(buttons.length);
     renderedButtons.forEach((b, i) => {
-      expect(b.contains(buttons[i].text)).toBeTruthy();
+      expect(b.contains(buttons[i].label)).toBeTruthy();
     });
   });
 
   it('renders without cityName', () => {
     const wrapper = wrap();
-    const renderedButtons = wrapper.find('Button');
+    const renderedButtons = wrapper.find('Field');
 
     expect(wrapper.find('TextAlignCenterHeading')).toHaveLength(1);
     expect(renderedButtons).toHaveLength(buttons.length);
     renderedButtons.forEach((b, i) => {
-      expect(b.contains(buttons[i].text)).toBeTruthy();
+      expect(b.contains(buttons[i].label)).toBeTruthy();
     });
   });
 });
