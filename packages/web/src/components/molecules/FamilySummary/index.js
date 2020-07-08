e991ca329e8d5a6205e2480b6f73c84acc51d207
Kunal Shah <knlshah@gmail.com>
2020-07-08 16:00:57 -0700

    not need in Family summary

--- packages/web/src/components/molecules/FamilySummary/index.js
+++ packages/web/src/components/molecules/FamilySummary/index.js
@@ -106,7 +106,7 @@ const FamilySummary = ({
       }
       {client.uuidAux && client.uuidAux.uuidInfo && client.uuidAux.uuidInfo.locationInfo &&
         <ColumWrapper>
-          <Label palette="grey">Family's Preferred location</Label>
+          <Label palette="grey">Preferred location</Label>
           <Block size="caption">{client.uuidAux.uuidInfo.locationInfo.city ? [client.uuidAux.uuidInfo.locationInfo.city, client.uuidAux.uuidInfo.locationInfo.state].filter(v => v).join(', ') : 'None'}</Block>
         </ColumWrapper>
       }
