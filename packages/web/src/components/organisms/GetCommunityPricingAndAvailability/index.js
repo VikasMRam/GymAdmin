d3e220684e3fdddec8add127c74e66c5ed7a9aff
Amal Francis <amal@seniorly.com>
2020-07-31 20:32:39 +0530

    assessment wizard changes

--- packages/web/src/components/organisms/GetCommunityPricingAndAvailability/index.js
+++ packages/web/src/components/organisms/GetCommunityPricingAndAvailability/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { string, func } from 'prop-types';
+import { string, func, bool } from 'prop-types';
+import styled from 'styled-components';
 
 import { community as communityPropType } from 'sly/common/propTypes/community';
 import pad from 'sly/web/components/helpers/pad';
 import fullWidth from 'sly/web/components/helpers/fullWidth';
-import { Button, Heading, Box } from 'sly/web/components/atoms';
+import { Button, Heading, Box, Icon } from 'sly/common/components/atoms';
 import CommunityRating from 'sly/web/components/molecules/CommunityRating';
 import CommunityPricing from 'sly/web/components/molecules/CommunityPricing';
 
-const StyledButton = fullWidth(Button);
+const StyledButton = fullWidth(styled(Button)`
+  display: grid;
+  grid-template-columns: min-content min-content;
+  grid-gap: ${size('spacing.medium')};
+`);
 
 const PaddedHeading = pad(Heading, 'large');
 
@@ -18,13 +23,14 @@ const PaddedCommunityRating = pad(CommunityRating);
 
 const onClickEvent = id => ({ action: 'click-gcp-button-sidebar', category: 'PricingWizard', label: id });
 
-const GetCommunityPricingAndAvailability = ({ community: { id, startingRate, rates, propRatings: { reviewsValue, numReviews } }, buttonTo, onClick }) => (
+const GetCommunityPricingAndAvailability = ({ completedAssessment, community: { id, startingRate, rates, propRatings: { reviewsValue, numReviews } }, buttonTo, onClick }) => (
   <Box>
     <PaddedHeading level="title" size="subtitle">Get Pricing and Availability</PaddedHeading>
     {startingRate > 0 && <PaddedCommunityPricing id={id} estimated={rates !== 'Provided'} price={startingRate} />}
     {reviewsValue > 0 && <PaddedCommunityRating rating={reviewsValue} numReviews={numReviews} />}
     <StyledButton to={buttonTo} onClick={onClick} event={onClickEvent(id)}>
-      Get Detailed Pricing
+      {completedAssessment && <Icon icon="tick" />}
+      {completedAssessment ? 'Pricing Requested' : 'Get Detailed Pricing'}
     </StyledButton>
   </Box>
 );
@@ -33,6 +39,7 @@ GetCommunityPricingAndAvailability.propTypes = {
   community: communityPropType,
   buttonTo: string,
   onClick: func,
+  completedAssessment: bool,
 };
 
 export default GetCommunityPricingAndAvailability;
