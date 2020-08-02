497c403885e19be0a808ad4cd77851c2822fd767
Amal Francis <amal@seniorly.com>
2020-08-02 21:33:35 +0530

    fix styled reference of Heading inside css

--- packages/common/src/components/atoms/Heading/index.js
+++ packages/common/src/components/atoms/Heading/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { oneOf, string } from 'prop-types';
+import styled from 'styled-components';
 
 import Root from './Root';
 
@@ -12,13 +13,14 @@ const getPad = (size) => {
   }
 };
 
-const Heading = props => (
+// styled wrapper is required to allow this component to be reffered in css tyles like ${Heading}: {}
+const Heading = styled(props => (
   <Root
     size={props.size || props.level}
     pad={getPad(props.size)}
     {...props}
   />
-);
+))``;
 
 Heading.propTypes = {
   level: oneOf(['hero', 'title', 'subtitle']).isRequired,
