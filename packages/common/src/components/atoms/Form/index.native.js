c0e9d57bd80d8bbfb1144ff7b139976315c70c1f
Amal Francis <amal@seniorly.com>
2020-08-06 12:59:48 +0530

    loginForm for both web and mobile

--- packages/common/src/components/atoms/Form/index.native.js
+++ packages/common/src/components/atoms/Form/index.native.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Children, isValidElement, cloneElement } from 'react';
+import { func, node } from 'prop-types';
 
 import { View } from 'sly/mobile/components/atoms';
 
-const Form = props => <View {...props} />;
+const Form = ({ onSubmit, children, ...props }) => {
+  const childrenWithProps = Children.map(children, (child) => {
+    if (isValidElement(child) && child.props.type === 'submit') {
+      return cloneElement(child, { onPress: onSubmit });
+    }
+
+    return child;
+  });
+
+  return <View {...props}>{childrenWithProps}</View>;
+};
+
+Form.propTypes = {
+  onSubmit: func,
+  children: node,
+};
 
 export default Form;
