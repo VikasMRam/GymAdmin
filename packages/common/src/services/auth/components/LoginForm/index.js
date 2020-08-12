e7875aae36e126c145bbeedc172ece0c174bda87
Amal Francis <amal@seniorly.com>
2020-08-13 00:19:05 +0530

    add CreatePasswordForm, LoginWithPasswordForm for web and mobile

--- packages/common/src/services/auth/components/LoginForm/index.js
+++ packages/common/src/services/auth/components/LoginForm/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { func, bool, string } from 'prop-types';
 import { Field } from 'redux-form';
 
+import { CUSTOMER_ROLE, AGENT_ND_ROLE } from 'sly/common/constants/roles';
+import rolePropType from 'sly/common/propTypes/role';
 import { Heading, Block, Button, Form } from 'sly/common/components/atoms';
 import ButtonLink from 'sly/common/components/molecules/ButtonLink';
 import ReduxField from 'sly/common/components/organisms/ReduxField';
 
 const LoginForm = ({
-  handleSubmit, submitting, invalid, error, onResetPasswordClick, onRegisterClick,
+  handleSubmit, submitting, invalid, error, onResetPasswordClick, onRegisterClick, role,
 }) => (
   <Form onSubmit={handleSubmit}>
-    <Heading pad="xLarge" size="subtitle">Log in</Heading>
+    <Heading pad="xLarge" size="subtitle">{role === AGENT_ND_ROLE ? 'Seniorly Partner Agent Log in' : 'Log in'}</Heading>
     <Field
       name="email"
       label="Email"
@@ -32,12 +34,15 @@ const LoginForm = ({
       Log in
     </Button>
     {error && <Block pad="xLarge" palette="danger" size="caption">{error}</Block>}
-    <ButtonLink pad="large" align="center" palette="primary" size="caption" onClick={onResetPasswordClick}>
+    <ButtonLink pad="large" align="center" palette="primary" size="caption"  onClick={onResetPasswordClick}>
       Reset password
     </ButtonLink>
-    <Block align="center" direction="row" size="caption">
-      Don&apos;t have an account?&nbsp;&nbsp;
-      <ButtonLink palette="primary" onClick={onRegisterClick}>Sign up</ButtonLink>
+    <Block align="center" verticalAlign="middle" direction="row">
+      {role !== AGENT_ND_ROLE &&
+        <Block size="caption" marginRight="small">Don&apos;t have an account?</Block>}
+      <ButtonLink palette="primary" size="caption" onClick={onRegisterClick}>
+        {role === AGENT_ND_ROLE ? 'Register for an account' : 'Sign up'}
+      </ButtonLink>
     </Block>
   </Form>
 );
@@ -49,6 +54,11 @@ LoginForm.propTypes = {
   error: string,
   onResetPasswordClick: func,
   onRegisterClick: func,
+  role: rolePropType.isRequired,
+};
+
+LoginForm.defaultProps = {
+  role: CUSTOMER_ROLE,
 };
 
 export default LoginForm;
--- packages/common/src/services/auth/components/OtpLoginForm/index.js
+++ packages/common/src/services/auth/components/OtpLoginForm/index.js
@@ -4,50 +4,34 @@ import { Field } from 'redux-form';
 
 import { email } from 'sly/web/services/validation';
 import { phoneFormatter } from 'sly/web/services/helpers/phone';
-import pad from 'sly/web/components/helpers/pad';
-import fullWidth from 'sly/web/components/helpers/fullWidth';
-import cursor from 'sly/web/components/helpers/cursor';
+import { Heading, Button, Block, Form } from 'sly/common/components/atoms';
 import ReduxField from 'sly/common/components/organisms/ReduxField';
-import { Heading, Button, Block } from 'sly/web/components/atoms';
-import { textAlign } from 'sly/web/components/helpers/text';
-
-const StyledHeading = textAlign(pad(Heading));
-StyledHeading.displayName = 'StyledHeading';
-
-const FullWidthButton = fullWidth(Button);
-FullWidthButton.displayName = 'FullWidthButton';
-
-const LargePaddedFullWidthButton = pad(FullWidthButton, 'large');
-LargePaddedFullWidthButton.displayName = 'LargePaddedFullWidthButton';
-
-const SocialLoginError = pad(Block, 'large');
-SocialLoginError.displayName = 'SocialLoginError';
-
-const LoginWithPassword = cursor(textAlign(Block));
-LoginWithPassword.displayName = 'LoginWithPassword';
-
-const ResetPassword = cursor(Block);
-ResetPassword.displayName = 'ResetPassword';
-
-const getSubmitButton = (error, props = {}) =>
-  error ? <LargePaddedFullWidthButton {...props} /> : <FullWidthButton {...props} />;
 
 const OtpLoginForm = ({
   handleSubmit, submitting, invalid, error, emailOrPhone, onResendCodeClick,
 }) => (
-  <form onSubmit={handleSubmit}>
-    <StyledHeading size="subtitle">Enter the code sent to {!email(emailOrPhone) ? emailOrPhone : phoneFormatter(emailOrPhone, true)}</StyledHeading>
+  <Form onSubmit={handleSubmit}>
+    <Heading size="subtitle" pad="xLarge">
+      Enter the code sent to {!email(emailOrPhone) ? emailOrPhone : phoneFormatter(emailOrPhone, true)}
+    </Heading>
     <Field
       name="code"
       label="Code"
-      labelRight={<ResetPassword palette="primary" size="caption" onClick={onResendCodeClick}>Resend code</ResetPassword>}
+      labelRight={<Block cursor="pointer" palette="primary" size="caption" onClick={onResendCodeClick}>Resend code</Block>}
       type="text"
       placeholder="6-digit code"
       component={ReduxField}
     />
-    {getSubmitButton(error, { children: 'Log in', type: 'submit', disabled: submitting || invalid })}
+    <Button
+      type="submit"
+      width="100%"
+      disabled={submitting || invalid}
+      pad={error ? 'large' : 'xLarge'}
+    >
+      Log in
+    </Button>
     {error && <Block palette="danger" size="caption">{error}</Block>}
-  </form>
+  </Form>
 );
 
 OtpLoginForm.propTypes = {
