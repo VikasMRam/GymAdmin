46ff331085474ce1ba2c1bd68a958b3a7b03ca4a
Amal Francis <amal@seniorly.com>
2020-08-14 11:20:07 +0530

    add routes in mobile app

--- packages/mobile/src/components/App.js
+++ packages/mobile/src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-native';
 import { ThemeProvider } from 'styled-components';
 
 import theme from 'sly/common/components/themes/default';
 import LoginScreen from 'sly/mobile/components/screens/LoginScreen';
 
+const routes = [
+  {
+    path: '/',
+    component: LoginScreen,
+    exact: true,
+  },
+];
+
+const routeComponents = routes.map(({ component: Component, ...route }) => (
+  <Route
+    key={route.path}
+    {...route}
+    component={props => (
+      <Component {...props} />
+    )}
+  />
+));
+
 const App = () => (
   <ThemeProvider theme={theme}>
-    <LoginScreen />
+    <Switch>
+      {routeComponents}
+    </Switch>
   </ThemeProvider>
 );
 
