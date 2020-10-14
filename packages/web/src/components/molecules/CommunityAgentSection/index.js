0e1e9333bd78707ed8a8822713b84891d7cd52c7
Amal Francis <amal@seniorly.com>
2020-10-14 14:34:44 +0530

    update CommunityAgentSection

--- packages/web/src/components/molecules/CommunityAgentSection/index.js
+++ packages/web/src/components/molecules/CommunityAgentSection/index.js
@@ -20,7 +20,7 @@ const CommunityAgentSection = ({
 }) => {
   const { name } = agent;
   const {
-    bio, profileImageUrl, recentFamiliesHelped, experience,
+    profileImageUrl, recentFamiliesHelped, experience,
   } = agent.info;
 
   return (
@@ -34,7 +34,7 @@ const CommunityAgentSection = ({
         <Avatar size="xxxLarge" user={{ name, picture: { src: profileImageUrl } }} />
         <Block textAlign="left">
           <Block weight="medium" palette="slate">{name}</Block>
-          <Block palette="grey">{bio}</Block>
+          <Block palette="grey">Local Senior Living Expert</Block>
         </Block>
       </Grid>
       <Description pad="xLarge" dimensions={['max-content', 'max-content']} gap="xxLarge">
@@ -43,7 +43,9 @@ const CommunityAgentSection = ({
         {experience && <IconItem icon="favourite-light" iconPalette="slate">{experience} years of experience</IconItem>}
       </Description>
       <Heading size="body">What is a Local Senior Living Expert?</Heading>
-      <Block>Our Local Senior Living Experts specialize in guiding families through the entire process of finding the right senior living community for their loved one. They live locally and can share their knowledge of current pricing and availability for communities near you. Working with an Expert is a completely free service to you.</Block>
+      <Block>
+        Our Local Senior Living Experts specialize in guiding families through the entire process of finding the right senior living community for their loved one. They live locally and can share their knowledge of a community’s pricing, availability, amenities, and insights about the staff. They also know about current promotions and can even help negotiate rent. Working with an Expert is a completely free service to you.
+      </Block>
     </Block>
   );
 };
