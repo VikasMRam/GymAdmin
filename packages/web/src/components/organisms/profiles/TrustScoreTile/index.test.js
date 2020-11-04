af565bb43bfb395ae0267c5141d790669f4f9a00
Sushanth Ramakrishna <sushanthr@gmail.com>
2020-11-03 16:11:29 -0800

    Update test

--- packages/web/src/components/organisms/profiles/TrustScoreTile/index.test.js
+++ packages/web/src/components/organisms/profiles/TrustScoreTile/index.test.js
@@ -1,67 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import CommunityTile from '.';
+import TrustScoreTile from '.';
 
 import RhodaGoldmanPlaza from 'sly/storybook/sample-data/property-rhoda-goldman-plaza.json';
 
-const note = 'note test';
-const defaultProps = {
-  onSlideChange: jest.fn(),
+const rgsAux = {
+  rgsInfo: { trustScore: 95, scoreParams: { trustScore: 95, lastInspectionDate: new Date(), licensedDate: new Date()   } },
 };
-const wrap = (props = {}) => shallow(<CommunityTile community={RhodaGoldmanPlaza} {...defaultProps} {...props} />);
+const RhodaGoldmanPlazaWithAll = { ...RhodaGoldmanPlaza, rgsAux };
+const wrap = (props = {}) => shallow(<TrustScoreTile community={RhodaGoldmanPlazaWithAll} {...props} />);
 
-describe('CommunityTile', () => {
+describe('TrustTile', () => {
   it('renders', () => {
     const wrapper = wrap();
-    expect(wrapper.find('MediaGallery')).toHaveLength(1);
-    expect(wrapper.find('CommunityInfo')).toHaveLength(1);
-    expect(wrapper.find('Button')).toHaveLength(0);
-    expect(wrapper.find('[testID="EditNote"]')).toHaveLength(0);
-    expect(wrapper.find('[testID="AddNote"]')).toHaveLength(0);
-  });
-
-  it('renders note', () => {
-    const wrapper = wrap({ note });
-    expect(wrapper.find('[testID="Note"]').text()).toContain(note);
-  });
-
-  it('onEditNoteClick gets called', () => {
-    const onEditNoteClick = jest.fn();
-    const wrapper = wrap({ note, onEditNoteClick });
-
-    wrapper.find('[testID="EditNote"]').simulate('click');
-    expect(onEditNoteClick).toHaveBeenCalled();
-  });
-
-  it('onAddNoteClick gets called', () => {
-    const onAddNoteClick = jest.fn();
-    const wrapper = wrap({ onAddNoteClick, addNote: true });
-
-    wrapper.find('[testID="AddNote"]').simulate('click');
-    expect(onAddNoteClick).toHaveBeenCalled();
-  });
-
-  it('action buttons rendered', () => {
-    const actionButtons = [
-      {
-        text: 'Ask Question',
-        onClick: jest.fn(),
-      },
-      {
-        text: 'Blah',
-        ghost: true,
-        onClick: jest.fn(),
-      },
-    ];
-    const wrapper = wrap({ actionButtons });
-
-    expect(wrapper.find('CommunityInfo')).toHaveLength(1);
-    actionButtons.forEach((actionButton, i) => {
-      expect(wrapper.find('Button[testID="ActionButton"]').at(i).dive()
-        .text()).toBe(actionButton.text);
-      wrapper.find('Button[testID="ActionButton"]').at(i).simulate('click');
-      expect(actionButton.onClick).toHaveBeenCalled();
-    });
+    expect(wrapper.find('IconItem')).toHaveLength(3);
+    expect(wrapper.find('Link')).toHaveLength(1);
   });
 });
