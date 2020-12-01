2fd770cf5d58377192d0ee2b13cac5b302947d16
Kunal Shah <knlshah@gmail.com>
2020-12-01 06:33:38 -0800

    fix radius test

--- packages/web/src/components/search/helpers.test.js
+++ packages/web/src/components/search/helpers.test.js
@@ -8,30 +8,30 @@ const filterParams = {
 
 it('filter search params checks against searchWhiteListParmas correctly', () => {
   const fullParams = {
-    size: 'small', radius: 30, dummy: '', some: '',
+    size: 'small', dummy: '', some: '',
   };
-  expect(filterSearchParams(fullParams)).toEqual({ size: 'small', radius: 30 });
+  expect(filterSearchParams(fullParams)).toEqual({ size: 'small'});
 });
 
 it('type of care filters are transformed into url correctly', () => {
   // When the path and search params contain the same filters selected should be true
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { toc: 'assisted-living' }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?radius=20&size=small',
+      path: '/assisted-living/california/san-francisco?size=small',
       selected: true,
     });
 
   // When the path and search params contain different filters selected should be false
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { toc: 'independent-living' }))
     .toEqual({
-      path: '/independent-living/california/san-francisco?radius=20&size=small',
+      path: '/independent-living/california/san-francisco?size=small',
       selected: false,
     });
 
   baseParams.toc = 'nursing-homes';
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { toc: 'nursing-homes' }))
     .toEqual({
-      path: '/nursing-homes/california/san-francisco?radius=20&size=small',
+      path: '/nursing-homes/california/san-francisco?size=small',
       selected: true,
     });
 });
@@ -40,34 +40,34 @@ it('size & budget & radius filters are transformed into url correctly', () => {
   baseParams.toc = 'assisted-living';
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { budget: 2000 }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?budget=2000&radius=20&size=small',
+      path: '/assisted-living/california/san-francisco?budget=2000&size=small',
       selected: false,
     });
   //
   filterParams.budget = 3000;
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { budget: 2000 }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?budget=2000&radius=20&size=small',
+      path: '/assisted-living/california/san-francisco?budget=2000&size=small',
       selected: false,
     });
   //
   filterParams.budget = 4000;
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { budget: 4000 }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?radius=20&size=small',
+      path: '/assisted-living/california/san-francisco?size=small',
       selected: true,
     });
 
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { size: 'small' }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?budget=4000&radius=20',
+      path: '/assisted-living/california/san-francisco?budget=4000',
       selected: true,
     });
 
   filterParams.size = 'medium';
   expect(filterLinkPath({ ...baseParams, ...filterParams }, { size: 'small' }))
     .toEqual({
-      path: '/assisted-living/california/san-francisco?budget=4000&radius=20&size=small',
+      path: '/assisted-living/california/san-francisco?budget=4000&size=small',
       selected: false,
     });
 });
