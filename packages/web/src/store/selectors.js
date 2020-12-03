8789baf1f0e11f46574fc07f47ab6b7553c04031
Amal Francis <amal@seniorly.com>
2020-12-03 12:11:34 +0530

    some ChatBoxContainer cleanup

--- packages/web/src/store/selectors.js
+++ packages/web/src/store/selectors.js
@@ -1,11 +1,7 @@
 import * as experiments from './experiments/selectors';
-import * as chatBox from './chatBox/selectors';
 
 export const getExperiment = (state, experimentName) =>
   experiments.getExperimentByName(state.experiments, experimentName);
 
 export const getExperiments = state =>
   experiments.getExperiments(state.experiments);
-
-export const hasChatBoxFooterReached = state =>
-  chatBox.hasFooterReached(state.chatBox);
