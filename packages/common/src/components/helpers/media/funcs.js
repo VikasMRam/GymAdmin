7a2f4f8edaffb13d30357906a04543ec646b4524
Alfonso de la Osa <fonz@seniorly.com>
2021-02-09 21:37:11 +0000

    S1785 provide apiko with basic block functionality (#1956)

--- packages/common/src/components/helpers/media/funcs.js
+++ packages/common/src/components/helpers/media/funcs.js
@@ -2,11 +2,49 @@ import { css } from 'styled-components';
 
 import { size } from 'sly/common/components/themes';
 
+const responsiveStyles = [
+  'margin',
+  'marginTop',
+  'marginBottom',
+  'marginLeft',
+  'marginRight',
+  'marginX',
+  'marginY',
+  'm',
+  'mt',
+  'mb',
+  'ml',
+  'mr',
+  'pad',
+  'mx',
+  'my',
+  'padding',
+  'paddingTop',
+  'paddingBottom',
+  'paddingLeft',
+  'paddingRight',
+  'paddingX',
+  'paddingY',
+  'p',
+  'pt',
+  'pr',
+  'pb',
+  'pl',
+  'px',
+  'py',
+];
+
 const makeStyles = (styles) => {
   if (Array.isArray(styles) || typeof styles === 'string' || !styles) {
     return styles;
   }
-  return css(styles);
+  const entries = { ...styles };
+  responsiveStyles.forEach((style) => {
+    if (entries[style]) {
+      delete entries[style];
+    }
+  });
+  return css(entries);
 };
 
 export const upTo = (device, styles) => css`
--- packages/web/src/components/helpers/pad.js
+++ packages/web/src/components/helpers/pad.js
@@ -1,16 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { size } from 'sly/common/components/themes';
-
-export const withPad = ({ pad } = {}) => {
-  if (!pad) {
-    return null;
-  }
-
-  return css`
-    margin-bottom: ${size('spacing', pad)};
-  `;
-};
 
 const pad = (Component, which = 'xLarge') => styled(Component)`
   margin-bottom: ${size('spacing', which)};
