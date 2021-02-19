9644c70893d17e6317e9742891ed1c874f2d47e2
Ruslan <r.milinchuk@apiko.com>
2021-02-19 11:58:42 +0200

    Delete useless changes

--- packages/web/src/components/resourceCenter/Topic/index.js
+++ packages/web/src/components/resourceCenter/Topic/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import { object } from 'prop-types';
 
 import Block from 'sly/common/components/atoms/Block';
-import { usePrefetch } from 'sly/web/services/api/prefetch';
-import { topics } from 'sly/web/components/resourceCenter/helper';
-import Header from "sly/web/components/resourceCenter/Header";
 
-const Topic = ({ match }) => {
-  const { topic } = match.params;
-
-  const { requestInfo } = usePrefetch(
-    'getArticle',
-      req => req({ topic_eq: topics.find(({ value }) => value.replace(/\s/g, '-').toLowerCase() === topic)?.label || '' }));
-  return (
-    <Block>
-      <Header />
-      <Block>
-
-      </Block>
-    </Block>
-  );
-};
-
-Topic.propTypes = {
-  match: object,
-};
+const Topic = () => <Block>Coming soon</Block>;
 
 export default Topic;
