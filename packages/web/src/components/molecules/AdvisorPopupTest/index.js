77a2dea126006f98a947e05817e2f9b0d6a5347c
Sushanth Ramakrishna <sushanthr@gmail.com>
2021-03-09 14:01:18 -0800

    S1838 : Updated pricing email campaign - city page link working incorrectly

--- packages/web/src/components/molecules/AdvisorPopupTest/index.js
+++ packages/web/src/components/molecules/AdvisorPopupTest/index.js
@@ -3,33 +3,10 @@ import styled from 'styled-components';
 import { func } from 'prop-types';
 
 import agentPropType from 'sly/common/propTypes/agent';
-import { size } from 'sly/common/components/themes';
-import { assetPath } from 'sly/web/components/themes';
-import { getImagePath } from 'sly/web/services/images';
+// import { size } from 'sly/common/components/themes';
 import { phoneFormatter } from 'sly/web/services/helpers/phone';
-import { Block, Button, Image, Link } from 'sly/web/components/atoms';
-import IconItem from 'sly/web/components/molecules/IconItem';
+import { Block, Button, Link } from 'sly/web/components/atoms';
 import SlyEvent from 'sly/web/services/helpers/events';
-
-const SubHeading = styled(Block)`
-  margin-bottom: ${size('spacing.xLarge')};
-`;
-
-const items = [
-  { icon: 'favourite-light', text: '100% free. They do not charge you.' },
-  { icon: 'location', text: 'Personalized service. They set up tours and can accompany you.' },
-  { icon: 'book', text: 'Expert knowledge. They can answer any question you have.' },
-  { icon: 'house', text: 'Insider information. They know unique details about this community.' },
-];
-
-const List = styled.ul`
-  padding: 0 0 ${size('spacing.xLarge')};
-`;
-
-const ListItem = styled.li`
-  list-style: none;
-  margin: 0 0 ${size('spacing.large')};
-`;
 
 const GotItButton = styled(Button)`
   width: 100%;
