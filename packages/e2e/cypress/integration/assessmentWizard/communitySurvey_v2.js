ec43e094b40d5e1a51b5f964ca40007cbcb5bdf2
Sushanth Ramakrishna <sushanthr@gmail.com>
2021-04-30 11:16:05 -0700

    Fix adl options in test

--- packages/e2e/cypress/integration/assessmentWizard/communitySurvey_v2.js
+++ packages/e2e/cypress/integration/assessmentWizard/communitySurvey_v2.js
@@ -2,7 +2,7 @@ import { TEST_COMMUNITY } from '../../constants/community';
 import { responsive, waitForHydration } from '../../helpers/tests';
 import randomUser from '../../helpers/randomUser';
 
-import { SERVICES_OPTIONS, WHO_PERSON_OPTIONS, ADL_OPTIONS, TIMING_OPTIONS, BUDGET_OPTIONS, MEDICAID_OPTIONS }
+import {  EXPERIMENTAL_ADL_OPTIONS, MEDICAID_OPTIONS }
   from 'sly/web/constants/wizards/assessment';
 
 Cypress.on('uncaught:exception', () => {
@@ -121,7 +121,7 @@ describe('Community survey', () => {
                 parents: 'Does your parent need help with any of the following?',
                 other: 'Do you need help with any of the following?',
               },
-              Options: ADL_OPTIONS,
+              Options: EXPERIMENTAL_ADL_OPTIONS,
               maxSelect: 4,
               optionsId: 'adl',
               skipAllowed: true,
