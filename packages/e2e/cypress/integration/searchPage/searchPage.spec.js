33aef3d9206f22d1dd191d9bdf4cd4f22660c23f
Vikas M R <vikas@sandora.co>
2021-05-26 21:51:52 +0530

    map fix

--- packages/e2e/cypress/integration/searchPage/searchPage.spec.js
+++ packages/e2e/cypress/integration/searchPage/searchPage.spec.js
@@ -713,7 +713,7 @@ describe('Assisted Search Page Sections', () => {
         cy.stub(win, 'open').as('windowOpen');
       });
 
-      mapCheck([currentList], 'MARKER_NAVIGATION');
+      mapCheck(currentList, 'MARKER_NAVIGATION');
     });
   });
 });
