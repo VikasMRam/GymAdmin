d764d34349c130a9e013feee6880eb370104c3ba
Vikas M R <vikas@sandora.co>
2021-06-15 15:46:17 +0530

    accepting valid numbers for contract percentage

--- packages/web/src/components/organisms/DashboardCommunityAdminForm/index.js
+++ packages/web/src/components/organisms/DashboardCommunityAdminForm/index.js
@@ -6,7 +6,6 @@ import { Button } from 'sly/web/components/atoms';
 import EditField from 'sly/web/components/form/EditField';
 import { statuses } from 'sly/web/constants/communities';
 import { PROVIDER_ROLE_PARAM } from 'sly/common/constants/roles';
-
 import {
   Section,
   SectionActions,
@@ -28,152 +27,162 @@ export default class DashboardCommunityAdminForm extends Component {
     currentValues: object,
   };
 
-  render() {
-    const {
-      handleSubmit, invalid, submitting, canEdit, currentValues,
-    } = this.props;
+   contractPercentageValidator = (value) => {
+     return value && (value < 0 || value > 1) ? 'Value must be in between 0.0 - 1.0' : undefined;
+   }
 
-    const contractInfo = currentValues?.rgsAux?.rgsInfo?.contract_info || {};
-    const valueLabel = contractInfo.contractType === 'Percentage'
-      ? 'Value between 0.0 - 1.0'
-      : 'Value in dollars';
+   render() {
+     const {
+       handleSubmit, invalid, submitting, canEdit, currentValues,
+     } = this.props;
 
-    return (
-      <Section
-        as="form"
-        onSubmit={handleSubmit}
-      >
-        <SectionHeader>
-          Admin
-        </SectionHeader>
+     const contractInfo = currentValues?.rgsAux?.rgsInfo?.contract_info || {};
+     const valueLabel = contractInfo.contractType === 'Percentage'
+       ? 'Value between 0.0 - 1.0'
+       : 'Value in dollars';
 
-        <SectionForm heading="Metadata">
-          <EditField
-            name="user"
-            label="Primary User"
-            type="user"
-            role={PROVIDER_ROLE_PARAM}
-            readOnly={!canEdit}
-          />
-          <EditField
-            name="slyScore"
-            label="Sly Score"
-            type="number"
-            readOnly={!canEdit}
-          />
-          <EditField
-            name="status"
-            label="Status"
-            type="select"
-            readOnly={!canEdit}
-          >
-            <option>Select an option</option>
-            {statusOptions}
-          </EditField>
-          <EditField
-            name="tags"
-            label="Tags"
-            type="autocomplete"
-            readOnly={!canEdit}
-            column={tagColumn}
-            isMulti
-          />
-        </SectionForm>
-        <SectionForm heading="Community information">
-          <EditField
-            name="propInfo.promoTitle"
-            label="Promotions Title"
-            type="text"
-            readOnly={!canEdit}
-          />
-          <EditField
-            name="propInfo.promoDescription"
-            label="Promotions Description"
-            type="textarea"
-            readOnly={!canEdit}
-          />
-        </SectionForm>
-        <SectionForm heading="Notes">
-          <EditField
-            name="propInfo.adminNotes"
-            label="Admin Notes"
-            type="textarea"
-            readOnly={!canEdit}
-          />
-        </SectionForm>
-        <SectionForm heading="SEO">
-          <EditField
-            name="propInfo.websiteTitle"
-            label="Website Title Tag"
-            type="text"
-            readOnly={!canEdit}
-          />
-          <EditField
-            name="propInfo.websiteMetaDescription"
-            label="Website Meta Description"
-            type="text"
-            readOnly={!canEdit}
-          />
-        </SectionForm>
-        <SectionForm heading="Contract">
-          <EditField
-            name="rgsAux.rgsInfo.contract_info.hasContract"
-            label="Has contract"
-            type="boolean"
-            readOnly={!canEdit}
-          />
-          {contractInfo?.hasContract && (
-            <>
-              <EditField
-                label="Type of contract"
-                name="rgsAux.rgsInfo.contract_info.contractType"
-                type="choice"
-                options={[
+     const contractValueValidators = contractInfo.contractType === 'Percentage'
+       ? [this.contractPercentageValidator]
+       : [];
+
+     return (
+       <Section
+         as="form"
+         onSubmit={handleSubmit}
+       >
+         <SectionHeader>
+           Admin
+         </SectionHeader>
+
+         <SectionForm heading="Metadata">
+           <EditField
+             name="user"
+             label="Primary User"
+             type="user"
+             role={PROVIDER_ROLE_PARAM}
+             readOnly={!canEdit}
+           />
+           <EditField
+             name="slyScore"
+             label="Sly Score"
+             type="number"
+             readOnly={!canEdit}
+           />
+           <EditField
+             name="status"
+             label="Status"
+             type="select"
+             readOnly={!canEdit}
+           >
+             <option>Select an option</option>
+             {statusOptions}
+           </EditField>
+           <EditField
+             name="tags"
+             label="Tags"
+             type="autocomplete"
+             readOnly={!canEdit}
+             column={tagColumn}
+             isMulti
+           />
+         </SectionForm>
+         <SectionForm heading="Community information">
+           <EditField
+             name="propInfo.promoTitle"
+             label="Promotions Title"
+             type="text"
+             readOnly={!canEdit}
+           />
+           <EditField
+             name="propInfo.promoDescription"
+             label="Promotions Description"
+             type="textarea"
+             readOnly={!canEdit}
+           />
+         </SectionForm>
+         <SectionForm heading="Notes">
+           <EditField
+             name="propInfo.adminNotes"
+             label="Admin Notes"
+             type="textarea"
+             readOnly={!canEdit}
+           />
+         </SectionForm>
+         <SectionForm heading="SEO">
+           <EditField
+             name="propInfo.websiteTitle"
+             label="Website Title Tag"
+             type="text"
+             readOnly={!canEdit}
+           />
+           <EditField
+             name="propInfo.websiteMetaDescription"
+             label="Website Meta Description"
+             type="text"
+             readOnly={!canEdit}
+           />
+         </SectionForm>
+         <SectionForm heading="Contract">
+           <EditField
+             name="rgsAux.rgsInfo.contract_info.hasContract"
+             label="Has contract"
+             type="boolean"
+             readOnly={!canEdit}
+           />
+           {contractInfo?.hasContract && (
+           <>
+             <EditField
+               label="Type of contract"
+               name="rgsAux.rgsInfo.contract_info.contractType"
+               type="choice"
+               options={[
                   { value: 'Flat Rate', label: 'Flat rate' },
                   { value: 'Percentage', label: 'Percentage' },
                 ]}
-                readOnly={!canEdit}
-              />
-              <EditField
-                label={valueLabel}
-                name="rgsAux.rgsInfo.contract_info.value"
-                type="number"
-                inputmode="numeric"
-                readOnly={!canEdit}
-                parse={value => !value ? null : Number(value)}
-              />
-              <EditField
-                label="Contract Url"
-                name="rgsAux.rgsInfo.contract_info.url"
-                type="text"
-              />
-              <EditField
-                label="Contract Notes"
-                name="rgsAux.rgsInfo.contract_info.notes"
-                type="textarea"
-                readOnly={!canEdit}
-              />
-              <EditField
-                label="Contract Status"
-                name="rgsAux.rgsInfo.contract_info.contractStatus"
-                type="select"
-                readOnly={!canEdit}Only={!canEdit}
-              >
-                <option value="" disabled>Select an option</option>
-                <option value="No contract">No contract</option>
-                <option value="Pending">Pending</option>
-                <option value="Signed">Signed</option>
-              </EditField>
-            </>
+               readOnly={!canEdit}
+             />
+             <EditField
+               label={valueLabel}
+               name="rgsAux.rgsInfo.contract_info.value"
+               type="number"
+               inputmode="numeric"
+               readOnly={!canEdit}
+               parse={value => Number.isNaN(value) ? null : Number(value)}
+               validate={contractValueValidators}
+             />
+             <EditField
+               label="Contract Url"
+               name="rgsAux.rgsInfo.contract_info.url"
+               type="text"
+             />
+             <EditField
+               label="Contract Notes"
+               name="rgsAux.rgsInfo.contract_info.notes"
+               type="textarea"
+               readOnly={!canEdit}
+             />
+             <EditField
+               label="Contract Status"
+               name="rgsAux.rgsInfo.contract_info.contractStatus"
+               type="select"
+               readOnly={!canEdit}
+               Only={!canEdit}
+             >
+               <option value="" disabled>Select an option</option>
+               <option value="No contract">No contract</option>
+               <option value="Pending">Pending</option>
+               <option value="Signed">Signed</option>
+             </EditField>
+           </>
           )}
-        </SectionForm>
-        <SectionActions>
-          <Button type="submit" disabled={!canEdit || invalid || submitting}>
-            Save changes
-          </Button>
-        </SectionActions>
-      </Section>
-    );
-  }
+         </SectionForm>
+         <SectionActions>
+           <Button type="submit" disabled={!canEdit || invalid || submitting}>
+             Save changes
+           </Button>
+         </SectionActions>
+       </Section>
+     );
+   }
 }
 
