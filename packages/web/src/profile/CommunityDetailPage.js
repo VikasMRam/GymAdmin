671c018652507297b392fa18e29d6dbf6ac414b6
Ruslan <r.milinchuk@apiko.com>
2021-06-24 16:20:54 +0300

    Delete useless section

--- packages/web/src/profile/CommunityDetailPage.js
+++ packages/web/src/profile/CommunityDetailPage.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, useContext } from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import { object, func } from 'prop-types';
 import { ifProp } from 'styled-tools';
@@ -21,7 +21,7 @@ import {
 import pad from 'sly/web/components/helpers/pad';
 import { getIsActiveAdult, getPartnerAgent } from 'sly/web/services/helpers/community';
 import { getAgentFirstName } from 'sly/web/services/helpers/agents';
-import { Button, Link } from 'sly/common/components/atoms';
+import { Button } from 'sly/common/components/atoms';
 import { color, space, sx$tablet, sx$laptop, Hr, Block, font } from 'sly/common/system';
 import SeoLinks from 'sly/web/components/organisms/SeoLinks';
 import SampleMenu from 'sly/web/components/organisms/SampleMenu';
@@ -413,29 +413,6 @@ export default class CommunityDetailPage extends PureComponent {
 
                   </StyledHeadingBoxSection>
                 }
-
-                {rgsAux.rgsInfo && rgsAux.rgsInfo.resourceLinks && rgsAux.rgsInfo.resourceLinks.length > 0 && (
-                  <StyledHeadingBoxSection
-                    heading={`Helpful ${typeOfCare} Resources`}
-                  >
-                    {rgsAux.rgsInfo.resourceLinks.map((item, i) => (
-                      <>
-                        <Link
-                          href={item.to}
-                          palette="primary"
-                          event={{
-                            category: 'community-resource-link',
-                            action: 'link-click',
-                            label: item.to,
-                          }}
-                        >
-                          {item.title}
-                        </Link>
-                        {i !== rgsAux.rgsInfo.resourceLinks.length - 1 && <Hr />}
-                      </>
-                    ))}
-                  </StyledHeadingBoxSection>
-                )}
 
                 {reviews && reviews.length > 0 &&
                   <StyledHeadingBoxSection
