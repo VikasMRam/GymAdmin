2d631ccc814f422e1eb90f85818c6a939d752df5
Vikas M R <vikas@seniorly.com>
2021-07-29 09:27:23 +0530

    Added resident name step to wizard

--- packages/web/src/assessment/steps/ResidentName/index.js
+++ packages/web/src/assessment/steps/ResidentName/index.js
@@ -2,49 +2,54 @@ import React from 'react';
 import { func, number, bool } from 'prop-types';
 import { Field } from 'redux-form';
 
-import { Wrapper, Footer } from 'sly/web/assessment/Template';
+import { Wrapper, Footer, PageWrapper } from 'sly/web/assessment/Template';
 import { Heading, Box, Grid } from 'sly/common/components/atoms';
 import TipBox from 'sly/web/components/molecules/TipBox';
 import ReduxField from 'sly/common/components/organisms/ReduxField';
+import { Block } from 'sly/common/system';
 
 const ResidentName = ({
   handleSubmit, onSkipClick, numberOfPeople, invalid, submitting, hasTip,
 }) => (
-  <Wrapper hasSecondColumn={hasTip}>
-    <Box>
-      <Heading level="subtitle" weight="medium" pad="xLarge">
-        {numberOfPeople > 1 ?
+  <PageWrapper>
+    <Wrapper hasSecondColumn={hasTip}>
+      <Box>
+        <Heading level="subtitle" weight="medium" pad="xLarge">
+          {numberOfPeople > 1 ?
           'What are the residents\' names?' :
           'What is the resident\'s name?'}
-      </Heading>
-      <form onSubmit={handleSubmit}>
-        {Array(numberOfPeople).fill().map((_, i) => (
-          <Grid gap="regular" key={Symbol(i).toString()}>
-            <Field
-              name={`firstName${numberOfPeople > 1 ? i + 1 : ''}`}
-              type="text"
-              label="First Name"
-              component={ReduxField}
-              pad="xLarge"
-            />
-            <Field
-              name={`lastName${numberOfPeople > 1 ? i + 1 : ''}`}
-              type="text"
-              label="Last Name"
-              component={ReduxField}
-              pad="xLarge"
-            />
-          </Grid>
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          {Array(numberOfPeople).fill().map((_, i) => (
+            <Grid gap="regular" key={Symbol(i).toString()}>
+              <Field
+                name={`firstName${numberOfPeople > 1 ? i + 1 : ''}`}
+                type="text"
+                label="First Name"
+                component={ReduxField}
+                pad="xLarge"
+              />
+              <Field
+                name={`lastName${numberOfPeople > 1 ? i + 1 : ''}`}
+                type="text"
+                label="Last Name"
+                component={ReduxField}
+                pad="xLarge"
+              />
+            </Grid>
         ))}
-        <Footer onSkipClick={onSkipClick} invalid={invalid} submitting={submitting} />
-      </form>
-    </Box>
-    {hasTip &&
-      <TipBox heading="WHY THIS IS IMPORTANT:" height="fit-content">
-        We help every family as if they are our own.
-      </TipBox>
+          <Footer onSkipClick={onSkipClick} invalid={invalid} submitting={submitting} />
+        </form>
+      </Box>
+      {hasTip &&
+      <Block marginTop="l">
+        <TipBox heading="WHY THIS IS IMPORTANT:" height="fit-content" >
+          We help every family as if they are our own.
+        </TipBox>
+      </Block>
     }
-  </Wrapper>
+    </Wrapper>
+  </PageWrapper>
 );
 
 ResidentName.propTypes = {
