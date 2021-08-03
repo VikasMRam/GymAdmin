48e5ae8d34861408332dba853e427564ff405019
Vikas M R <vikas@seniorly.com>
2021-08-03 16:12:14 +0530

    e2e for resident name step

--- packages/e2e/cypress/integration/assessmentWizard/communitySurvey_v2.js
+++ packages/e2e/cypress/integration/assessmentWizard/communitySurvey_v2.js
@@ -222,6 +222,19 @@ describe('Community survey', () => {
         expect(attrs).to.have.property('actionPage', `/wizards/assessment/community/${community.id}`);
         expect(attrs).to.have.property('actionType', 'wizardStepCompleted');
       });
+      // Post Conversion Checks
+      // Check for resident name step
+      waitForHydration(cy.contains("What is the resident's name?", { timeout: 30000 }));
+      waitForHydration(cy.get('form input[id=fullName]')).type(name);
+      waitForHydration(cy.get('form button[type=submit]').contains('Continue')).click({ force: true });
+      cy.wait('@postUuidActions').then((xhr) => {
+        const request = xhr.requestBody;
+        const attrs = request.data.attributes;
+        expect(attrs.actionInfo).to.have.property('stepName', 'step-12:ResidentName');
+        expect(attrs.actionInfo).to.have.property('wizardName', 'assessmentWizard');
+        expect(attrs.actionInfo).to.have.property('wizardPostConversionInfo', true);
+      });
+
       waitForHydration(cy.contains('We\'ve sent your request!', { timeout: 30000 }));
     });
   });
