98d51f55d62edebd8c4351f632b3d9b08d4cb3bd
Kunal Shah <knlshah@gmail.com>
2021-08-10 16:25:57 -0700

    missed search

--- packages/web/src/services/helpers/adtiles.js
+++ packages/web/src/services/helpers/adtiles.js
@@ -4,6 +4,7 @@ import { tocPaths, urlize } from 'sly/web/services/helpers/url';
 export const ccrcAdTileStates = ['AZ', 'CT', 'DC', 'DE', 'HI', 'ID', 'IN', 'KS', 'KY', 'LA', 'MN', 'NE', 'NH', 'OH', 'OK', 'RI', 'TX', 'UT', 'WA', 'WI', 'MO', 'NY', 'NC'];
 
 export const shouldShowZillowSearchAd = (toc) => {
+  return false;
   const validTocs = tocs.filter(e =>
     e.value.match(/active-adult|independent-living|continuing-care-retirement-community/)).map(e => e.value);
   return validTocs.indexOf(toc) > -1;
@@ -11,7 +12,7 @@ export const shouldShowZillowSearchAd = (toc) => {
 
 export const  shouldShowZillowProfileAd = (community) => {
   //disable zillow adtile.
-  return false
+  return false;
   // return true;
   // S1303 : Zillow Ad Tiles on CCRC Do Not Send List
   // https://airtable.com/tblt2MRAZThT31Ee9/viwlPQXuHxbH2unIj/recfsTZBjUSGl0TUe?blocks=hide
